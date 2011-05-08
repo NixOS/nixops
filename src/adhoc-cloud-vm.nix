e0882271bdbef3c51a50d2c8f6af4ad9b440b515
Eelco Dolstra <e.dolstra@tudelft.nl>
2011-05-08 21:45:31 +0000

    * Move.

---
+++ src/adhoc-cloud-vm.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }:
+
+with pkgs.lib;
+
+{
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "/dev/vda";
+  boot.initrd.kernelModules = [ "virtio_blk" "virtio_pci" ];
+
+  fileSystems =
+    [ { mountPoint = "/";
+        label = "nixos";
+      }
+    ];
+
+  swapDevices = [ { label = "swap"; } ];
+
+  networking.hostName = mkOverride 950 "";
+  networking.firewall.enable = mkOverride 950 true;
+
+  services.openssh.enable = true;
+
+  services.mingetty.ttys = [ "hvc0" "tty1" "tty2" ];
+}
