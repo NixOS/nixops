08ef256515fff7040c44a2743d86bac1dacdbed3
Eelco Dolstra <e.dolstra@tudelft.nl>
2011-10-21 15:50:27 +0000

    * Hm, maybe key pairs should be specified in an environment variable.

--- examples/ec2.nix
+++ examples/ec2.nix
@@ -12,7 +12,7 @@
       deployment.ec2.controller = https://ec2.eu-west-1.amazonaws.com:443/;
       deployment.ec2.ami = "ami-ecb49e98";
       deployment.ec2.instanceType = "m1.large";
-      deployment.ec2.keyPair = "gsg-keypair";
+      deployment.ec2.keyPair = "eelco";
     };
 
 }
