0192af0c31658c7f54ceb2e36b02c14804b3d701
Eelco Dolstra <e.dolstra@tudelft.nl>
2011-10-25 04:07:14 +0200

    * Use the new "zone" option.

--- examples/ec2.nix
+++ examples/ec2.nix
@@ -9,8 +9,7 @@
       services.httpd.documentRoot = "${pkgs.valgrind}/share/doc/valgrind/html";
       
       deployment.targetEnv = "ec2";
-      deployment.ec2.controller = https://ec2.us-east-1.amazonaws.com:443/;
-      deployment.ec2.ami = "ami-d93bf4b0";
+      deployment.ec2.zone = "us-east-1";
       deployment.ec2.instanceType = "m1.large";
       deployment.ec2.keyPair = "eelco";
       deployment.ec2.securityGroup = "eelco-test";
