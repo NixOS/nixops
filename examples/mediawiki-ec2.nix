5a873635a52a6f0678bacb638cfe0e79bd6f1c6b
Eelco Dolstra <e.dolstra@tudelft.nl>
2011-11-02 22:28:50 -0400

    * DRBD hackery.

--- examples/mediawiki-ec2.nix
+++ examples/mediawiki-ec2.nix
@@ -1,12 +1,62 @@
-{
+let
 
-  webserver = 
+  drbdConf = nodes:
+    ''
+      global {
+        usage-count no;
+      }
+
+      resource r0 {
+        protocol C;
+        syncer {
+          verify-alg sha1;
+        }
+        floating ${nodes.webserver.config.networking.privateIPv4}:7789 {
+          device    /dev/drbd1;
+          disk      /dev/loop0;
+          meta-disk internal;
+        }
+        floating ${nodes.webserver_failover.config.networking.privateIPv4}:7789 {
+          device    /dev/drbd1;
+          disk      /dev/loop0;
+          meta-disk internal;
+        }
+      }
+    '';
+
+  loopUp = pkgs:
+    { name = "loop-up";
+      task = true;
+      startOn = "mounted MOUNTPOINT=/ephemeral0";
+      script = "${pkgs.utillinux}/sbin/losetup /dev/loop0 /ephemeral0/backing && start drbd-up";
+    };
+
+in {
+
+  webserver =
+    { pkgs, nodes, ... }:
     {
       deployment.targetEnv = "ec2";
       deployment.ec2.region = "us-east-1";
       deployment.ec2.instanceType = "m1.large";
       deployment.ec2.keyPair = "eelco";
       deployment.ec2.securityGroups = [ "eelco-test" ];
+      services.drbd.enable = true;
+      services.drbd.config = drbdConf nodes;
+      jobs.loopUp = loopUp pkgs;
+    };
+
+  webserver_failover = 
+    { pkgs, nodes, ... }:
+    {
+      deployment.targetEnv = "ec2";
+      deployment.ec2.region = "us-east-1";
+      deployment.ec2.instanceType = "m1.large";
+      deployment.ec2.keyPair = "eelco";
+      deployment.ec2.securityGroups = [ "eelco-test" ];
+      services.drbd.enable = true;
+      services.drbd.config = drbdConf nodes;
+      jobs.loopUp = loopUp pkgs;
     };
 
 }
