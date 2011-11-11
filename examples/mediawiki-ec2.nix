dbba9f912e97fe7fbb405f680431586482095b50
Eelco Dolstra <e.dolstra@tudelft.nl>
2011-11-11 17:25:06 -0500

    Add a distributed Mediawiki example
    In this example, the PostgreSQL database is on another machine.  This
    means that the schema initialisation done by the NixOS Mediawiki
    module doesn't work.  It's done on the database server after
    PostgreSQL has started, so there are no issues with the machine start
    order.  TODO: refactor this wrt the Mediawiki module.

---
+++ examples/mediawiki-ec2.nix
@@ -0,0 +1,16 @@
+let
+
+  config =
+    { deployment.targetEnv = "ec2";
+      deployment.ec2.region = "us-east-1";
+      deployment.ec2.instanceType = "m1.large";
+      deployment.ec2.keyPair = "eelco";
+      deployment.ec2.securityGroups = [ "eelco-test" ];
+    };
+
+in
+
+{
+  webserver = config;
+  database = config;
+}
