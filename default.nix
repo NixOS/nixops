b4a6d25f51ac40dece8df358ea7280132df6081b
Eelco Dolstra <e.dolstra@tudelft.nl>
2012-03-12 11:35:33 +0100

    Python skeleton

--- default.nix
+++ default.nix
@@ -1,28 +1,11 @@
 with import <nixpkgs> {};
 
-stdenv.mkDerivation {
+pythonPackages.buildPythonPackage {
   name = "charon";
 
   src = lib.cleanSource ./.;
 
-  buildInputs =
-    [ perl makeWrapper perlPackages.XMLLibXML perlPackages.JSON
-      perlPackages.TextTable perlPackages.ListMoreUtils
-      perlPackages.NetAmazonEC2 perlPackages.FileSlurp
-      perlPackages.DataUUID perlPackages.SetObject
-      #nixUnstable
-    ];
+  doCheck = false;
 
-  installPhase = 
-    ''
-      mkdir -p $out/bin
-      cp src/charon.pl $out/bin/charon
-
-      mkdir -p $out/share/nix/charon
-      cp nix/*.nix nix/id* $out/share/nix/charon/ # urgh!
-
-      wrapProgram $out/bin/charon \
-        --set PERL5LIB $PERL5LIB \
-        --prefix NIX_PATH : charon=$out/share/nix/charon
-    ''; # */
+  installCommand = "python setup.py install --prefix=$out";
 }
