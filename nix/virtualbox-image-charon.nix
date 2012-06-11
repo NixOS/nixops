3a3c858306505c63656348c0c982eb27fe3178da
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-06-11 11:16:27 -0400

    Minimise GRUB boot delay

--- nix/virtualbox-image-charon.nix
+++ nix/virtualbox-image-charon.nix
@@ -24,6 +24,8 @@ in
 
   boot.vesa = false;
 
+  boot.loader.grub.timeout = 1;
+
   # VirtualBox doesn't seem to lease IP addresses persistently, so we
   # may get a different IP address if dhcpcd is restarted.  So don't
   # restart dhcpcd.
