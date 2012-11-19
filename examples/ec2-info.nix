5f485ae25f0bcc1f663cd7221e7459b2e36d91b9
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-11-19 12:24:19 +0100

    Rename ec2-info.nix

--- examples/ec2-info.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, ... }:
-
-with pkgs.lib;
-
-{ deployment.ec2.accessKeyId = "AKIAIEMEJZVNOOHWZKZQ";
-  deployment.ec2.keyPair = mkDefault "eelco";
-  deployment.ec2.privateKey = mkDefault "/home/eelco/.ec2/logicblox/id_rsa-eelco-logicblox-${config.deployment.ec2.region}";
-  deployment.ec2.securityGroups = mkDefault [ "eelco-test" ];
-}
