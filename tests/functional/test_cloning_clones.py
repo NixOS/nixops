150341d81e2da5adf46726a2efe2349fc5590ce6
Shea Levy <shea@shealevy.com>
2012-12-12 15:00:29 -0500

    Add test for Deployment.clone

---
+++ tests/functional/test_cloning_clones.py
@@ -0,0 +1,10 @@
+from nose import tools
+
+from tests.functional import single_machine_test
+
+class TestCloningClones(single_machine_test.SingleMachineTest):
+    def run_check(self):
+        depl = self.depl.clone()
+        tools.assert_equal(depl.nix_exprs, self.depl.nix_exprs)
+        tools.assert_equal(depl.nix_path, self.depl.nix_path)
+        tools.assert_equal(depl.args, self.depl.args)
