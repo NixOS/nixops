2e32aece74e8311158bbfa2eda6e24cc8ac294fd
Shea Levy <shea@shealevy.com>
2012-12-13 19:13:54 -0500

    destroy_vms is generalized to destroy_resources

--- tests/__init__.py
+++ tests/__init__.py
@@ -14,7 +14,7 @@ def setup():
 def destroy(db, uuid):
     depl = deployment.open_deployment(db, uuid)
     depl.auto_response = "y"
-    depl.destroy_vms()
+    depl.destroy_resources()
 
 def teardown():
     try:
