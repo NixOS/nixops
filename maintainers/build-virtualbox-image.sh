24e09113fc3f97bc20cb9083781b196632699d83
Eelco Dolstra <eelco.dolstra@logicblox.com>
2013-01-24 17:04:44 +0100

    Small VirtualBox image generation fix

--- maintainers/build-virtualbox-image.sh
+++ maintainers/build-virtualbox-image.sh
@@ -1,7 +1,8 @@
 #! /bin/sh -e
+export NIXOS_CONFIG=/home/eelco/Dev/charon/nix/virtualbox-image-charon.nix
 version=$(nix-instantiate --eval-only '<nixos>' -A config.system.nixosVersion | sed s/'"'//g)
 echo "version = $version"
-NIXOS_CONFIG=/home/eelco/Dev/charon/nix/virtualbox-image-charon.nix nix-build '<nixos>' -A config.system.build.virtualBoxImage
+nix-build '<nixos>' -A config.system.build.virtualBoxImage
 name=virtualbox-charon-$version.vdi.xz
 xz < ./result/disk.vdi > $name
 scp -p $name root@lucifer:/data/releases/nixos/virtualbox-charon-images/
