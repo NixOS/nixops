77cdad039f7bec0df31a5084238f47cbfad2e7f5
Shea Levy <shea@shealevy.com>
2013-01-31 13:09:45 -0500

    Use a file lock for known_hosts.
    It might be preferable to create a known_hosts file in the deployment tempdir and add a flag to ssh invocations to use that, but this is easier for now.

--- charon/known_hosts.py
+++ charon/known_hosts.py
@@ -1,10 +1,10 @@
 import os
 import sys
 import threading
+import fcntl
 
 
-# Allow only one thread to rewrite known_hosts at a time.  TODO: use a
-# file lock.
+# Allow only one thread to rewrite known_hosts at a time.
 lock = threading.Lock()
 
 
@@ -13,25 +13,27 @@ def _rewrite(ip_address, public_host_key):
         path = os.path.expanduser("~/.ssh/known_hosts")
         if not os.path.isfile(path): return
     
-        f = open(path, 'r')
-        contents = f.read()
-        f.close()
+        with open(os.path.expanduser("~/.ssh/.known_hosts.lock"), 'w') as lockfile:
+            fcntl.flock(lockfile, fcntl.LOCK_EX) #unlock is implicit at the end of the with
+            f = open(path, 'r')
+            contents = f.read()
+            f.close()
 
-        def rewrite(l):
-            (names, rest) = l.split(' ', 1)
-            new_names = [ n for n in names.split(',') if n != ip_address ]
-            return ','.join(new_names) + " " + rest if new_names != [] else None
+            def rewrite(l):
+                (names, rest) = l.split(' ', 1)
+                new_names = [ n for n in names.split(',') if n != ip_address ]
+                return ','.join(new_names) + " " + rest if new_names != [] else None
 
-        new = [ l for l in [ rewrite(l) for l in contents.splitlines() ] if l is not None ]
+            new = [ l for l in [ rewrite(l) for l in contents.splitlines() ] if l is not None ]
 
-        if public_host_key:
-            new.append(ip_address + " " + public_host_key)
+            if public_host_key:
+                new.append(ip_address + " " + public_host_key)
 
-        tmp = "{0}.tmp-{1}".format(path, os.getpid())
-        f = open(tmp, 'w')
-        f.write('\n'.join(new + [""]))
-        f.close()
-        os.rename(tmp, path)
+            tmp = "{0}.tmp-{1}".format(path, os.getpid())
+            f = open(tmp, 'w')
+            f.write('\n'.join(new + [""]))
+            f.close()
+            os.rename(tmp, path)
 
 
 def remove(ip_address):
