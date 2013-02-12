bd1f289524f210b67427a2b8e5a908575df1b8fd
Eelco Dolstra <eelco.dolstra@logicblox.com>
2013-02-12 15:59:17 +0100

    Check whether the EC2 region name is valid

--- charon/ec2_utils.py
+++ charon/ec2_utils.py
@@ -38,8 +38,11 @@ def connect(region, access_key_id):
     """Connect to the specified EC2 region using the given access key."""
     assert region
     (access_key_id, secret_access_key) = fetch_aws_secret_key(access_key_id)
-    return boto.ec2.connect_to_region(
+    conn = boto.ec2.connect_to_region(
         region_name=region, aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key)
+    if not conn:
+        raise Exception("invalid EC2 region ‘{0}’".format(region))
+    return conn
 
 
 def get_access_key_id():
