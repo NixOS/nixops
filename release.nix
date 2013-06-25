a09a9c15b5c5e681edc54017fdcee84b2d37ee62
Eelco Dolstra <eelco.dolstra@logicblox.com>
2013-06-25 12:32:49 +0200

    Move default.nix into release.nix
    Also move generation of the docs into the "tarball" job.

--- release.nix
+++ release.nix
@@ -14,16 +14,30 @@ rec {
 
   tarball = pkgs.releaseTools.sourceTarball {
     name = "nixops-tarball";
+
     src = nixopsSrc;
+
     inherit version;
+
     officialRelease = true; # hack
-    buildInputs = [ pkgs.git ];
+
+    buildInputs = [ pkgs.git pkgs.libxslt ];
+
     postUnpack = ''
       # Clean up when building from a working tree.
       (cd $sourceRoot && (git ls-files -o | xargs -r rm -v))
     '';
+
     distPhase =
       ''
+        # Generate the manual and the man page.
+        cp ${import ./doc/manual { revision = nixopsSrc.rev; }} doc/manual/machine-options.xml
+        ${pkgs.lib.concatMapStrings (fn: ''
+          cp ${import ./doc/manual/resource.nix { revision = nixopsSrc.rev; module = ./nix + ("/" + fn + ".nix"); }} doc/manual/${fn}-options.xml
+        '') [ "sqs-queue" "ec2-keypair" "s3-bucket" "iam-role" ]}
+
+        make -C doc/manual docbookxsl=${pkgs.docbook5_xsl}/xml/xsl/docbook
+
         substituteInPlace scripts/nixops --subst-var-by version ${version}
         substituteInPlace setup.py --subst-var-by version ${version}
 
@@ -33,13 +47,54 @@ rec {
         rm -rf ../$releaseName/.git
         mkdir $out/tarballs
         tar  cvfj $out/tarballs/$releaseName.tar.bz2 -C .. $releaseName
+
+        echo "doc manual $out/share/doc/nixops manual.html" >> $out/nix-support/hydra-build-products
       '';
   };
 
-  build = import ./default.nix {
-    version = tarball.version;
-    revision = nixopsSrc.rev;
-  };
+  build = pkgs.lib.genAttrs [ "x86_64-linux" "i686-linux" ] (system:
+    with import <nixpkgs> {};
+
+    pythonPackages.buildPythonPackage rec {
+      name = "nixops-${version}";
+      namePrefix = "";
+
+      src = "${tarball}/tarballs/*.tar.bz2";
+
+      buildInputs = [ pythonPackages.nose pythonPackages.coverage ];
+
+      propagatedBuildInputs =
+        [ pythonPackages.prettytable
+          pythonPackages.boto
+          pythonPackages.sqlite3
+        ];
+
+      # For "nix-build --run-env".
+      postHook = ''
+        export PYTHONPATH=$(pwd):$PYTHONPATH
+        export PATH=$(pwd)/scripts:$PATH
+      '';
+
+      doCheck = false;
+
+      postInstall =
+        ''
+          # Backward compatibility symlink.
+          ln -s nixops $out/bin/charon
+
+          make -C doc/manual install \
+            docdir=$out/share/doc/nixops mandir=$out/share/man
+
+          mkdir -p $out/share/nix/nixops
+          cp -av nix/* $out/share/nix/nixops
+
+          mkdir -p $out/nix-support
+          echo "nix-build none $out" >> $out/nix-support/hydra-build-products
+          echo "doc manual $out/share/doc/nixops manual.html" >> $out/nix-support/hydra-build-products
+        ''; # */
+
+      meta.description = "Nix package for ${stdenv.system}";
+    });
 
   tests.none_backend = (import ./tests/none-backend.nix {
     nixops = build;
