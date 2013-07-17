56cf1d2138937838272c06570fcc951300487e6a
aszlig <aszlig@redmoonstudios.org>
2013-07-17 05:27:44 +0200

    hetzner: Truncate Robot's server name to 50 chars.
    We want to avoid exceptions as the Robot unly allows 50 characters at
    most. Sadly, even shorter hostnames will be truncated and might clutter
    up the server list a bit.
    
    But I've already sent a feature request to the Robot team at Hetzner, so
    we might have longer names soon.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- nixops/backends/hetzner.py
+++ nixops/backends/hetzner.py
@@ -423,7 +423,10 @@ class HetznerState(MachineState):
             self._install_base_system()
             server = self._get_server_by_ip(self.main_ipv4)
             vm_id = "nixops-{0}-{1}".format(self.depl.uuid, self.name)
-            server.set_name(vm_id)
+            # XXX: Truncated to 50 chars until the Robot allows more.
+            #      And this also means, that this field is unreliable so we
+            #      can't use it for uniquely identifying machine UUIDs.
+            server.set_name(vm_id[:50])
             self.vm_id = vm_id
             known_hosts.remove(self.main_ipv4)
             self.just_installed = True
