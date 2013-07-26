d11209c584960822110fdb44d80d9daa0c1103f2
aszlig <aszlig@redmoonstudios.org>
2013-07-26 20:30:38 +0200

    tests: Propagate commandline arguments to nose.
    This makes it way easier to skip certain tests and to provide other
    options specific to nose tests.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- coverage-tests.py
+++ coverage-tests.py
@@ -6,4 +6,8 @@ if __name__ == "__main__":
     assert os.getenv("EC2_SECURITY_GROUP") is not None, "The EC2_SECURITY_GROUP env var must be set to the name of an ec2 security group with inbound ssh access"
     assert os.getenv("EC2_KEY_PAIR") is not None, "The EC2_KEY_PAIR env var must be set to the name of an ec2 keypair"
     assert os.getenv("EC2_PRIVATE_KEY_FILE") is not None, "The EC2_PRIVATE_KEY_FILE env var must be set to the private key of an ec2 keypair"
-    nose.main(argv=[ sys.argv[0], "--with-xunit", "--with-coverage", "--cover-inclusive", "--cover-xml", "--cover-xml-file=coverage.xml", "--cover-html", "--cover-html-dir=./html", "--cover-package=nixops", "-e", "^tests\.py$" ])
+    nose.main(argv=[sys.argv[0], "--with-xunit", "--with-coverage",
+                    "--cover-inclusive", "--cover-xml",
+                    "--cover-xml-file=coverage.xml", "--cover-html",
+                    "--cover-html-dir=./html", "--cover-package=nixops",
+                    "-e", "^tests\.py$"] + sys.argv[1:])
