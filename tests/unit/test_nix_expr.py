c3b93091783c5ccbada0fd32d969d47196e31139
aszlig <aszlig@redmoonstudios.org>
2013-08-20 15:43:32 +0200

    test_nix_pprint: Add and imlement assert_nix().
    This method first of all shortens the line length and the main purpose
    is to print a multi-line error if the assertion doesn't hold. That way
    it's easier to spot what went wrong.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- tests/unit/test_nix_expr.py
+++ tests/unit/test_nix_expr.py
@@ -6,48 +6,54 @@ from nixops.nix_expr import py2nix, RawValue
 
 
 class Py2NixTest(unittest.TestCase):
+    def assert_nix(self, nix_expr, expected, maxwidth=80):
+        result = py2nix(nix_expr, maxwidth=maxwidth)
+        self.assertEqual(result, expected,
+                         "Expected:\n{0}\nGot:\n{1}".format(expected,
+                                                            result))
+
     def test_numeric(self):
-        self.assertEqual(py2nix(123), "123")
-        self.assertEqual(py2nix(-123), "builtins.sub 0 123")
+        self.assert_nix(123, "123")
+        self.assert_nix(-123, "builtins.sub 0 123")
         self.assertRaises(ValueError, py2nix, 123.4)
 
     def test_boolean(self):
-        self.assertEqual(py2nix(True), "true")
-        self.assertEqual(py2nix(False), "false")
+        self.assert_nix(True, "true")
+        self.assert_nix(False, "false")
 
     def test_null(self):
-        self.assertEqual(py2nix(None), "null")
+        self.assert_nix(None, "null")
 
     def test_invalid(self):
         self.assertRaises(ValueError, py2nix, lambda: 123)
         self.assertRaises(ValueError, py2nix, Exception)
 
     def test_string(self):
-        self.assertEqual(py2nix("xyz"), '"xyz"')
-        self.assertEqual(py2nix("a'b\"c"), r'''"a'b\"c"''')
-        self.assertEqual(py2nix("abc\ndef\nghi"), r'"abc\ndef\nghi"')
-        self.assertEqual(py2nix("abc\ndef\nghi\n", maxwidth=0),
-                         "''\n  abc\n  def\n  ghi\n''")
-        self.assertEqual(py2nix("\\foo"), r'"\\foo"')
-        self.assertEqual(py2nix("xx${yy}zz"), r'"xx\${yy}zz"')
-        self.assertEqual(py2nix("xx\n${yy}\nzz\n", maxwidth=0),
-                         "''\n  xx\n  ''${yy}\n  zz\n''")
-        self.assertEqual(py2nix("xx\n''yy\nzz\n", maxwidth=0),
-                         "''\n  xx\n  '''yy\n  zz\n''")
+        self.assert_nix("xyz", '"xyz"')
+        self.assert_nix("a'b\"c", r'''"a'b\"c"''')
+        self.assert_nix("abc\ndef\nghi", r'"abc\ndef\nghi"')
+        self.assert_nix("abc\ndef\nghi\n", "''\n  abc\n  def\n  ghi\n''",
+                        maxwidth=0)
+        self.assert_nix("\\foo", r'"\\foo"')
+        self.assert_nix("xx${yy}zz", r'"xx\${yy}zz"')
+        self.assert_nix("xx\n${yy}\nzz\n", "''\n  xx\n  ''${yy}\n  zz\n''",
+                        maxwidth=0)
+        self.assert_nix("xx\n''yy\nzz\n", "''\n  xx\n  '''yy\n  zz\n''",
+                        maxwidth=0)
 
     def test_raw_value(self):
-        self.assertEqual(py2nix({'a': RawValue('import <something>')}),
-                         '{ a = import <something>; }')
-        self.assertEqual(py2nix([RawValue("!")]),
-                         '[ ! ]')
+        self.assert_nix({'a': RawValue('import <something>')},
+                        '{ a = import <something>; }')
+        self.assert_nix([RawValue("!")], '[ ! ]')
 
     def test_list(self):
-        self.assertEqual(py2nix([1, 2, 3]), '[ 1 2 3 ]')
-        self.assertEqual(py2nix(["a", "b", "c"]), '[ "a" "b" "c" ]')
-        self.assertEqual(py2nix(["a\na\na\n", "b\nb\n", "c"]),
-                         r'[ "a\na\na\n" "b\nb\n" "c" ]')
-        self.assertEqual(py2nix(["a\na\na\n", "b\nb\n", "c"], maxwidth=15),
-                         '[\n  "a\\na\\na\\n"\n  "b\\nb\\n"\n  "c"\n]')
+        self.assert_nix([1, 2, 3], '[ 1 2 3 ]')
+        self.assert_nix(["a", "b", "c"], '[ "a" "b" "c" ]')
+        self.assert_nix(["a\na\na\n", "b\nb\n", "c"],
+                        r'[ "a\na\na\n" "b\nb\n" "c" ]')
+        self.assert_nix(["a\na\na\n", "b\nb\n", "c"],
+                        '[\n  "a\\na\\na\\n"\n  "b\\nb\\n"\n  "c"\n]',
+                        maxwidth=15)
 
     def test_nested_list(self):
         match = dedent('''
@@ -81,11 +87,11 @@ class Py2NixTest(unittest.TestCase):
         ]
         ''').strip()
 
-        self.assertEqual(py2nix([
+        self.assert_nix([
             [1, 2, 3],
             [4, 5, 6],
             [[6, 6, 6], [[7, 7, 7], [8, 8, 8], [9, 9, 9]]]
-        ], maxwidth=12), match)
+        ], match, maxwidth=12)
 
     def test_nested_singletons(self):
         match = dedent('''
@@ -96,22 +102,22 @@ class Py2NixTest(unittest.TestCase):
         ] ] ]
         ''').strip()
 
-        self.assertEqual(py2nix([[[1, 2, [[3]]]]], maxwidth=12), match)
+        self.assert_nix([[[1, 2, [[3]]]]], match, maxwidth=12)
 
     def test_attrkeys(self):
-        self.assertEqual(py2nix({'aaa': 123}), '{ aaa = 123; }')
-        self.assertEqual(py2nix({'a.a': 123}), '{ "a.a" = 123; }')
-        self.assertEqual(py2nix({'\\': 123}), r'{ "\\" = 123; }')
-        self.assertEqual(py2nix({'a1': 123}), '{ a1 = 123; }')
-        self.assertEqual(py2nix({'1a': 123}), '{ "1a" = 123; }')
-        self.assertEqual(py2nix({'_aa': 123}), '{ _aa = 123; }')
+        self.assert_nix({'aaa': 123}, '{ aaa = 123; }')
+        self.assert_nix({'a.a': 123}, '{ "a.a" = 123; }')
+        self.assert_nix({'\\': 123}, r'{ "\\" = 123; }')
+        self.assert_nix({'a1': 123}, '{ a1 = 123; }')
+        self.assert_nix({'1a': 123}, '{ "1a" = 123; }')
+        self.assert_nix({'_aa': 123}, '{ _aa = 123; }')
         self.assertRaises(KeyError, py2nix, {'': 123})
         self.assertRaises(KeyError, py2nix, {123: 123})
 
     def test_attrvalues(self):
-        self.assertEqual(py2nix({'a': "abc"}), '{ a = "abc"; }')
-        self.assertEqual(py2nix({'a': "a\nb\nc\n"}), r'{ a = "a\nb\nc\n"; }')
-        self.assertEqual(py2nix({'a': [1, 2, 3]}), r'{ a = [ 1 2 3 ]; }')
+        self.assert_nix({'a': "abc"}, '{ a = "abc"; }')
+        self.assert_nix({'a': "a\nb\nc\n"}, r'{ a = "a\nb\nc\n"; }')
+        self.assert_nix({'a': [1, 2, 3]}, r'{ a = [ 1 2 3 ]; }')
 
     def test_nested_attrsets(self):
         match = dedent('''
@@ -135,7 +141,7 @@ class Py2NixTest(unittest.TestCase):
         }
         ''').strip()
 
-        self.assertEqual(py2nix({
+        self.assert_nix({
             'aaa': {
                 'bbb': {
                     'ccc': 123,
@@ -146,4 +152,4 @@ class Py2NixTest(unittest.TestCase):
             'yyy': {
                 'y1': {'y2': {'y3': ["a", "b", {'c': 'd'}]}},
             },
-        }, maxwidth=0), match)
+        }, match, maxwidth=0)
