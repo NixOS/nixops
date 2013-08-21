30e6474ab5feecc489e0d2ca97036fe36982e3aa
aszlig <aszlig@redmoonstudios.org>
2013-08-22 01:45:43 +0200

    nix_expr: Fix docstring for py2nix().
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- nixops/nix_expr.py
+++ nixops/nix_expr.py
@@ -96,7 +96,8 @@ def _fold_string(value, rules):
 def py2nix(value, initial_indentation=0, maxwidth=80):
     """
     Return the given value as a Nix expression string.
-    IF initial_indentation is to a specific level (two spaces per level), don't
+
+    If initial_indentation is to a specific level (two spaces per level), don't
     inline fewer than that. Also, 'maxwidth' specifies the maximum line width
     which is enforced whenever it is possible to break an expression. Set to 0
     if you want to break on every occasion possible.
