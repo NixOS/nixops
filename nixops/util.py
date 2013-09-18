a2718b65615ec4a71a0f819db5fa675632e62f34
Rob Vermaas <rob.vermaas@gmail.com>
2013-09-18 16:36:39 +0200

    Make newly generated keypair use ecdsa by default (was: dsa).

--- nixops/util.py
+++ nixops/util.py
@@ -217,7 +217,7 @@ def attr_property(name, default, type=str):
     return property(get, set)
 
 
-def create_key_pair(key_name="NixOps auto-generated key", type="dsa"):
+def create_key_pair(key_name="NixOps auto-generated key", type="ecdsa"):
     key_dir = tempfile.mkdtemp(prefix="nixops-tmp")
     res = subprocess.call(["ssh-keygen", "-t", type, "-f", key_dir + "/key", "-N", '', "-C", key_name],
                           stdout=devnull)
