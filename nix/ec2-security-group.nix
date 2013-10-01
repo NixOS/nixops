030d21e9cb7f03261ab4ea01f7b7010d10fa5863
Shea Levy <shea@shealevy.com>
2013-10-01 14:04:47 -0400

    Initial interface for security groups as resources
    Signed-off-by: Shea Levy <shea@shealevy.com>

---
+++ nix/ec2-security-group.nix
@@ -0,0 +1,82 @@
+{ config, pkgs, uuid, name, ... }:
+
+with pkgs.lib;
+
+{
+
+  options = {
+
+    name = mkOption {
+      default = "charon-${uuid}-${name}";
+      type = types.uniq types.string;
+      description = "Name of the security group.";
+    };
+
+    description = mkOption {
+      default = "nixops-provisioned group ${name}";
+      type = types.string;
+      description = "Informational description of the security group";
+    };
+
+    accessKeyId = mkOption {
+      type = types.uniq types.string;
+      description = "The AWS Access Key ID.";
+    };
+
+    groupId = mkOption {
+      type = types.uniq types.string;
+      description = "The security group ID. This is set by NixOps.";
+    };
+
+    rules = mkOption {
+      type = types.listOf types.optionSet;
+      description = "The security group's rules";
+      default = {};
+      options = {
+        protocol = mkOption {
+          default = "tcp";
+          description = "The protocol (tcp, udp, or icmp) that this rule describes";
+          type = types.uniq types.string;
+        };
+
+        fromPort = mkOption {
+          description = "The bottom of the allowed port range for this rule (TCP/UDP only)";
+          type = types.uniq types.int;
+        };
+
+        toPort = mkOption {
+          description = "The top of the allowed port range for this rule (TCP/UDP only)";
+          type = types.uniq types.int;
+        };
+
+        typeNumber = mkOption {
+          description = "ICMP type number (ICMP only, -1 for all)";
+          type = types.uniq types.int;
+        };
+
+        codeNumber = mkOption {
+          description = "ICMP code number (ICMP only, -1 for all)";
+          type = types.uniq types.int;
+        };
+
+        sourceGroup = {
+          userId = mkOption {
+            description = "The AWS account ID that owns the source security group";
+            type = types.uniq types.string;
+          };
+
+          groupName = mkOption {
+            description = "The name of the source security group";
+            type = types.uniq types.string;
+          };
+        };
+
+        cidrIp = mkOption {
+          description = "The source IP range (CIDR notation)";
+          type = types.uniq types.string;
+        };
+      };
+    };
+  };
+
+}
