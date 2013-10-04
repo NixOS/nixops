d08bea0c5de70b2392650a1128f423b145bb3e85
Rob Vermaas <rob.vermaas@gmail.com>
2013-10-04 09:06:09 +0200

    Update us-east-1 hvm to be able to use all ephemeral devices.

--- nix/ec2.nix
+++ nix/ec2.nix
@@ -127,7 +127,7 @@ let
     "eu-west-1".hvm = "ami-0bafbf7f";
     "us-east-1".ebs = "ami-efcebb86";
     "us-east-1".s3  = "ami-6534400c";
-    "us-east-1".hvm = "ami-89c8bde0";
+    "us-east-1".hvm = "ami-5f491f36";
     "us-west-1".ebs = "ami-75f1df30";
     "us-west-1".s3  = "ami-aff1dfea";
     "us-west-1".hvm = "ami-e1f1dfa4";
