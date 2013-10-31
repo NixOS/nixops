d73e8af42b80c89dc9576640ad1548c1ba151227
Eelco Dolstra <eelco.dolstra@logicblox.com>
2013-10-31 13:38:25 +0100

    Put the AMI IDs in a separate file to make it easier to refer to

---
+++ nix/ec2-amis.nix
@@ -0,0 +1,14 @@
+{
+  "eu-west-1".ebs = "ami-8badbdff";
+  "eu-west-1".s3  = "ami-83afbff7";
+  "eu-west-1".hvm = "ami-0bafbf7f";
+  "us-east-1".ebs = "ami-efcebb86";
+  "us-east-1".s3  = "ami-6534400c";
+  "us-east-1".hvm = "ami-5f491f36";
+  "us-west-1".ebs = "ami-75f1df30";
+  "us-west-1".s3  = "ami-aff1dfea";
+  "us-west-1".hvm = "ami-e1f1dfa4";
+  "us-west-2".ebs = "ami-fd0998cd";
+  "us-west-2".s3  = "ami-870899b7";
+  "us-west-2".hvm = "ami-7d08994d";
+}
