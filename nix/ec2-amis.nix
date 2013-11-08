203e18e4421c361351129b2344e620da01b6b670
Rob Vermaas <rob.vermaas@gmail.com>
2013-11-08 12:11:27 +0100

    Update AMIs to nixos-13.10

--- nix/ec2-amis.nix
+++ nix/ec2-amis.nix
@@ -1,14 +1,14 @@
 {
-  "eu-west-1".ebs = "ami-8badbdff";
-  "eu-west-1".s3  = "ami-83afbff7";
-  "eu-west-1".hvm = "ami-0bafbf7f";
-  "us-east-1".ebs = "ami-efcebb86";
-  "us-east-1".s3  = "ami-6534400c";
-  "us-east-1".hvm = "ami-5f491f36";
-  "us-west-1".ebs = "ami-75f1df30";
-  "us-west-1".s3  = "ami-aff1dfea";
-  "us-west-1".hvm = "ami-e1f1dfa4";
-  "us-west-2".ebs = "ami-fd0998cd";
-  "us-west-2".s3  = "ami-870899b7";
-  "us-west-2".hvm = "ami-7d08994d";
+  "eu-west-1".ebs = "ami-f703e180";
+  "eu-west-1".s3  = "ami-4900e23e";
+  "eu-west-1".hvm = "ami-8b00e2fc";
+  "us-east-1".ebs = "ami-5bc39832";
+  "us-east-1".s3  = "ami-21c09b48";
+  "us-east-1".hvm = "ami-d1c09bb8";
+  "us-west-1".ebs = "ami-80f9cfc5";
+  "us-west-1".s3  = "ami-6ee6d02b";
+  "us-west-1".hvm = "ami-04e6d041";
+  "us-west-2".ebs = "ami-7429b144";
+  "us-west-2".s3  = "ami-482cb478";
+  "us-west-2".hvm = "ami-9a2cb4aa";
 }
