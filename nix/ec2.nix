95670c9a035a387afe458fbbcd8beea5746f5d82
Evgeny Egorochkin <phreedom@yandex.ru>
2014-06-23 14:21:43 +0300

    EC2: Instance: Disks: fix passing encryption config to auto-lux.nix

--- nix/ec2.nix
+++ nix/ec2.nix
@@ -422,7 +422,7 @@ in
 
     deployment.ec2.blockDeviceMapping = mkFixStrictness (listToAttrs
       (map (fs: nameValuePair (dmToDevice fs.device)
-        { inherit (fs.ec2) disk size deleteOnTermination encrypt passphrase iops;
+        { inherit (fs.ec2) disk size deleteOnTermination encrypt cipher keySize passphrase iops;
           fsType = if fs.fsType != "auto" then fs.fsType else fs.ec2.fsType;
         })
        (filter (fs: fs.ec2 != null) (attrValues config.fileSystems))));
