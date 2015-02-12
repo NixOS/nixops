3e401d29afd291afc15c83c2de43f3c5662d0a67
Bas van Dijk <v.dijk.bas@gmail.com>
2015-02-12 18:03:15 +0100

    Reenable the assert self.public_ipv4 in get_ssh_name

--- nixops/backends/none.py
+++ nixops/backends/none.py
@@ -34,6 +34,7 @@ class NoneState(MachineState):
         self.target_host = defn._target_host
 
     def get_ssh_name(self):
+        assert self.public_ipv4
         return self.public_ipv4
 
     @property
