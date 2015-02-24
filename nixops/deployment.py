ca91aea71b3c841947e9478b06b32d30e78338f6
aszlig <aszlig@redmoonstudios.org>
2015-02-24 17:58:40 +0100

    deployment: Avoid Nix daemon in rescue state.
    While in Hetzner rescue system, we don't have an instance of Nix daemon
    running, because the bootstrapper is only faking all Nix usage from the
    host system and running it within the chroot. Deploying to that chroot
    with NIX_REMOTE set to daemon thus won't work.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- nixops/deployment.py
+++ nixops/deployment.py
@@ -701,7 +701,8 @@ class Deployment(object):
 
             try:
                 # Set the system profile to the new configuration.
-                setprof = 'NIX_REMOTE=daemon nix-env -p /nix/var/nix/profiles/system --set "{0}"'
+                daemon_var = '' if m.state == RESCUE else 'NIX_REMOTE=daemon '
+                setprof = daemon_var + 'nix-env -p /nix/var/nix/profiles/system --set "{0}"'
                 if always_activate or self.definitions[m.name].always_activate:
                     m.run_command(setprof.format(m.new_toplevel))
                 else:
