266931ba169c7539f1d8df1c13ac96ab842e8129
Domen Kožar <domen@dev.si>
2015-12-15 23:17:54 +0100

    pep8

--- nixops/backends/libvirtd.py
+++ nixops/backends/libvirtd.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 
+from distutils import spawn
+import os
+import random
+import string
+import subprocess
+import time
+
 from nixops.backends import MachineDefinition, MachineState
 import nixops.util
-import sys
-from xml.etree import ElementTree
-import subprocess
-import os
-from distutils import spawn
-import string
-import time
-import random
+
 
 class LibvirtdDefinition(MachineDefinition):
     """Definition of a trivial machine."""
@@ -27,9 +27,11 @@ class LibvirtdDefinition(MachineDefinition):
         self.image_dir = x.find("attr[@name='imageDir']/string").get("value")
         assert self.image_dir is not None
 
-        self.networks = [ k.get("value")
-                            for k in x.findall("attr[@name='networks']/list/string") ]
+        self.networks = [
+            k.get("value")
+            for k in x.findall("attr[@name='networks']/list/string")]
         assert len(self.networks) > 0
+
 
 class LibvirtdState(MachineState):
     private_ipv4 = nixops.util.attr_property("privateIpv4", None)
@@ -59,10 +61,10 @@ class LibvirtdState(MachineState):
         return "nixops-{0}-{1}".format(self.depl.uuid, self.name)
 
     def _generate_primary_mac(self):
-        mac = [ 0x52, 0x54, 0x00,
-                random.randint(0x00, 0x7f),
-                random.randint(0x00, 0xff),
-                random.randint(0x00, 0xff) ]
+        mac = [0x52, 0x54, 0x00,
+               random.randint(0x00, 0x7f),
+               random.randint(0x00, 0xff),
+               random.randint(0x00, 0xff)]
         self.primary_mac = ':'.join(map(lambda x: "%02x" % x, mac))
 
     def create(self, defn, check, allow_reboot, allow_recreate):
@@ -79,21 +81,18 @@ class LibvirtdState(MachineState):
         if self.vm_id is None:
             os.putenv("NIXOPS_LIBVIRTD_PUBKEY", self.client_public_key)
             base_image = self._logged_exec(
-                        ["nix-build"]
-                        + self.depl._eval_flags(self.depl.nix_exprs) +
-                        ["--arg", "checkConfigurationOptions", "false",
-                         "-A", "nodes.{0}.config.deployment.libvirtd.baseImage"
-                                    .format(self.name),
-                         "-o", "{0}/libvirtd-image-{1}"
-                                    .format(self.depl.tempdir, self.name)],
-                        capture_stdout=True).rstrip()
+                ["nix-build"] + self.depl._eval_flags(self.depl.nix_exprs) +
+                ["--arg", "checkConfigurationOptions", "false",
+                 "-A", "nodes.{0}.config.deployment.libvirtd.baseImage".format(self.name),
+                 "-o", "{0}/libvirtd-image-{1}".format(self.depl.tempdir, self.name)],
+                capture_stdout=True).rstrip()
 
             if not os.path.exists(defn.image_dir):
-                    os.makedirs(defn.image_dir)
+                os.makedirs(defn.image_dir)
 
             self.disk_path = self._disk_path(defn)
-            self._logged_exec(["qemu-img", "create", "-f", "qcow2", "-b"
-                                    , base_image + "/disk.qcow2", self.disk_path])
+            self._logged_exec(["qemu-img", "create", "-f", "qcow2", "-b",
+                               base_image + "/disk.qcow2", self.disk_path])
             # TODO: use libvirtd.extraConfig to make the image accessible for your user
             os.chmod(self.disk_path, 0666)
             self.vm_id = self._vm_id()
@@ -112,42 +111,47 @@ class LibvirtdState(MachineState):
                 return '<mac address="' + self.primary_mac + '" />'
             else:
                 return ""
+
         def iface(n):
             return "\n".join([
-                      '    <interface type="network">'
-                    , maybe_mac(n)
-                    , '      <source network="{0}"/>'
-                    , '    </interface>'
-                    ]).format(n)
+                '    <interface type="network">',
+                maybe_mac(n),
+                '      <source network="{0}"/>',
+                '    </interface>',
+            ]).format(n)
 
         domain_fmt = "\n".join([
-                    '<domain type="kvm">'
-                    , '  <name>{0}</name>'
-                    , '  <memory unit="MiB">{1}</memory>'
-                    , '  <vcpu>1</vcpu>'
-                    , '  <os>'
-                    , '    <type arch="x86_64">hvm</type>'
-                    , '  </os>'
-                    , '  <devices>'
-                    , '    <emulator>{2}</emulator>'
-                    , '    <disk type="file" device="disk">'
-                    , '      <driver name="qemu" type="qcow2"/>'
-                    , '      <source file="{3}"/>'
-                    , '      <target dev="hda"/>'
-                    , '    </disk>'
-                    , "\n".join([ iface(n) for n in defn.networks ])
-                    , '    <graphics type="sdl" display=":0.0"/>'
-                    , '    <input type="keyboard" bus="usb"/>'
-                    , '    <input type="mouse" bus="usb"/>'
-                    , '  </devices>'
-                    , '</domain>'
-                    ])
+            '<domain type="kvm">',
+            '  <name>{0}</name>',
+            '  <memory unit="MiB">{1}</memory>',
+            '  <vcpu>1</vcpu>',
+            '  <os>',
+            '    <type arch="x86_64">hvm</type>',
+            '  </os>',
+            '  <devices>',
+            '    <emulator>{2}</emulator>',
+            '    <disk type="file" device="disk">',
+            '      <driver name="qemu" type="qcow2"/>',
+            '      <source file="{3}"/>',
+            '      <target dev="hda"/>',
+            '    </disk>',
+            '\n'.join([iface(n) for n in defn.networks]),
+            '    <graphics type="sdl" display=":0.0"/>',
+            '    <input type="keyboard" bus="usb"/>',
+            '    <input type="mouse" bus="usb"/>',
+            '  </devices>',
+            '</domain>',
+        ])
 
         qemu_kvm = spawn.find_executable("qemu-kvm")
-        assert qemu_kvm != None
+        assert qemu_kvm is not None, "qemu-kvm executable not found"
 
-        return domain_fmt.format(self._vm_id(), defn.memory_size
-                    , qemu_kvm, self._disk_path(defn))
+        return domain_fmt.format(
+            self._vm_id(),
+            defn.memory_size,
+            qemu_kvm,
+            self._disk_path(defn),
+        )
 
     def _parse_lease(self):
         for line in open("/var/lib/libvirt/dnsmasq/{0}.leases".format(self.primary_net)):
@@ -168,7 +172,7 @@ class LibvirtdState(MachineState):
         self.log_end(" " + self.private_ipv4)
 
     def _is_running(self):
-        ls = subprocess.check_output(["virsh", "-c", "qemu:///system", "list" ])
+        ls = subprocess.check_output(["virsh", "-c", "qemu:///system", "list"])
         return (string.find(ls, self.vm_id) != -1)
 
     def start(self):
@@ -181,7 +185,7 @@ class LibvirtdState(MachineState):
         else:
             dom_file = self.depl.tempdir + "/{0}-domain.xml".format(self.name)
             nixops.util.write_file(dom_file, self.domain_xml)
-            self._logged_exec(["virsh", "-c", "qemu:///system", "create", dom_file ]);
+            self._logged_exec(["virsh", "-c", "qemu:///system", "create", dom_file])
             self._wait_for_ip(l_time)
 
     def get_ssh_name(self):
@@ -191,10 +195,11 @@ class LibvirtdState(MachineState):
     def stop(self):
         assert self.vm_id
         if self._is_running():
-            self._logged_exec(["virsh", "-c", "qemu:///system", "destroy", self.vm_id ]);
+            self._logged_exec(["virsh", "-c", "qemu:///system", "destroy", self.vm_id])
 
     def destroy(self, wipe=False):
-        if not self.vm_id: return True
+        if not self.vm_id:
+            return True
         self.stop()
         if (self.disk_path and os.path.exists(self.disk_path)):
             os.unlink(self.disk_path)
