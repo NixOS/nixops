13a15b7e690e98e3adc1eb2af4151231f4b0d47c
Evgeny Egorochkin <phreedom@yandex.ru>
2016-01-03 15:41:34 +0200

    azure: public ip: port to ARM API

--- nix/azure-reserved-ip-address.nix
+++ nix/azure-reserved-ip-address.nix
@@ -4,7 +4,7 @@ with lib;
 with (import ./lib.nix lib);
 {
 
-  options = (import ./azure-credentials.nix lib "reserved IP address") // {
+  options = (import ./azure-mgmt-credentials.nix lib "reserved IP address") // {
 
     name = mkOption {
       example = "my-public-ip";
@@ -13,20 +13,57 @@ with (import ./lib.nix lib);
       description = "Description of the Azure reserved IP address. This is the <literal>Name</literal> tag of the address.";
     };
 
+    resourceGroup = mkOption {
+      example = "xxx-my-group";
+      type = types.either types.str (resource "azure-resource-group");
+      description = "The name or resource of an Azure resource group to create the IP address in.";
+    };
+
     location = mkOption {
       example = "West US";
       type = types.str;
+      description = "The Azure data center where the reserved IP address should be located.";
+    };
+
+    tags = mkOption {
+      default = {};
+      example = { environment = "production"; };
+      type = types.attrsOf types.str;
+      description = "Tag name/value pairs to associate with the IP address.";
+    };
+
+    idleTimeout = mkOption {
+      default = 4;
+      example = 30;
+      type = types.int;
       description = ''
-        The Azure data center where the reserved IP address should be located.
-        This should be the same location that is assigned to the cloud service
-        containing the deployment that will use the reserved IP address.
+          The timeout for the TCP idle connection.
+          The value can be set between 4 and 30 minutes.
       '';
     };
 
-    label = mkOption {
-      default = "";
-      type = types.str;
-      description = "Human-friendly label for the reserved IP address up to 100 characters in length.";
+    domainNameLabel = mkOption {
+      default = null;
+      example = "mylabel";
+      type = types.nullOr types.str;
+      description = ''
+          The concatenation of the domain name label and the regionalized DNS
+          zone make up the fully qualified domain name associated with the
+          public IP address. If a domain name label is specified, an A DNS
+          record is created for the public IP in the Microsoft Azure DNS
+          system. Example FQDN: mylabel.northus.cloudapp.azure.com.
+      '';
+    };
+
+    reverseFqdn = mkOption {
+      default = null;
+      example = "mydomain.com";
+      type = types.nullOr types.str;
+      description = ''
+          A fully qualified domain name that resolves to this public IP address.
+          If the reverseFqdn is specified, then a PTR DNS record is created pointing
+          from the IP address in the in-addr.arpa domain to the reverse FQDN.
+      '';
     };
 
   };
--- nixops/resources/azure_reserved_ip_address.py
+++ nixops/resources/azure_reserved_ip_address.py
@@ -9,6 +9,7 @@ import time
 from nixops.util import attr_property
 from nixops.azure_common import ResourceDefinition, ResourceState
 
+from azure.mgmt.network import *
 
 class AzureReservedIPAddressDefinition(ResourceDefinition):
     """Definition of an Azure Reserved IP Address"""
@@ -25,8 +26,12 @@ class AzureReservedIPAddressDefinition(ResourceDefinition):
         ResourceDefinition.__init__(self, xml)
 
         self.reserved_ip_address_name = self.get_option_value(xml, 'name', str)
+        self.copy_option(xml, 'resourceGroup', 'resource')
         self.copy_option(xml, 'location', str)
-        self.copy_option(xml, 'label', str)
+        self.copy_tags(xml)
+        self.copy_option(xml, 'idleTimeout', int)
+        self.copy_option(xml, 'domainNameLabel', str, optional = True)
+        self.copy_option(xml, 'reverseFqdn', str, optional = True)
 
     def show_type(self):
         return "{0} [{1}]".format(self.get_type(), self.location)
@@ -36,10 +41,14 @@ class AzureReservedIPAddressState(ResourceState):
     """State of an Azure Reserved IP Address"""
 
     reserved_ip_address_name = attr_property("azure.name", None)
+    resource_group = attr_property("azure.resourceGroup", None)
     location = attr_property("azure.location", None)
-    label = attr_property("azure.label", None)
+    tags = attr_property("azure.tags", {}, 'json')
+    idle_timeout = attr_property("azure.idleTimeout", None, int)
+    domain_name_label = attr_property("azure.domainNameLabel", None)
+    reverse_fqdn = attr_property("azure.reverseFqdn", None)
+
     ip_address = attr_property("azure.ipAddress", None)
-    azure_id = attr_property("azure.id", None)
 
     @classmethod
     def get_type(cls):
@@ -69,22 +78,45 @@ class AzureReservedIPAddressState(ResourceState):
 
     def get_resource(self):
         try:
-            return self.sms().get_reserved_ip_address(self.reserved_ip_address_name)
+            return self.nrpc().public_ip_addresses.get(
+                      self.resource_group, self.resource_id).public_ip_address
         except azure.common.AzureMissingResourceHttpError:
             return None
 
     def destroy_resource(self):
-        req = self.sms().delete_reserved_ip_address(self.reserved_ip_address_name)
-        self.finish_request(req)
+        self.nrpc().public_ip_addresses.delete(self.resource_group, self.resource_id)
 
-    defn_properties = [ 'label', 'location' ]
+    def is_settled(self, resource):
+        return resource is None or (resource.provisioning_state == 'Succeeded')
+
+    defn_properties = [ 'location', 'tags', 'idle_timeout', 'domain_name_label', 'reverse_fqdn' ]
+
+    def _update_resource(self, defn):
+        dns_settings = PublicIpAddressDnsSettings(
+                                domain_name_label = defn.domain_name_label,
+                                reverse_fqdn = defn.reverse_fqdn
+                       ) if defn.domain_name_label or defn.reverse_fqdn else None
+        self.nrpc().public_ip_addresses.create_or_update(
+                        defn.resource_group, defn.reserved_ip_address_name,
+                        PublicIpAddress(
+                            location = defn.location,
+                            public_ip_allocation_method = 'Static',
+                            idle_timeout_in_minutes = defn.idle_timeout,
+                            tags = defn.tags,
+                            dns_settings = dns_settings
+                        ))
+        self.state = self.UP
+        self.copy_properties(defn)
+        self.ip_address = self.get_settled_resource().ip_address
+
 
     def create(self, defn, check, allow_reboot, allow_recreate):
         self.no_property_change(defn, 'location')
-        self.no_property_change(defn, 'label')
+        self.no_property_change(defn, 'resource_group')
 
-        self.copy_credentials(defn)
+        self.copy_mgmt_credentials(defn)
         self.reserved_ip_address_name = defn.reserved_ip_address_name
+        self.resource_group = defn.resource_group
 
         if check:
             address = self.get_settled_resource()
@@ -92,9 +124,14 @@ class AzureReservedIPAddressState(ResourceState):
                 self.warn_missing_resource()
             elif self.state == self.UP:
                 self.handle_changed_property('location', address.location, can_fix = False)
-                self.handle_changed_property('label', address.label, can_fix = False)
-                self.handle_changed_property('ip_address', address.address, property_name = '')
-                self.handle_changed_property('azure_id', address.id, can_fix = False)
+                self.handle_changed_property('tags', address.tags)
+                self.handle_changed_property('ip_address', address.ip_address, property_name = '')
+                self.handle_changed_property('idle_timeout', address.idle_timeout_in_minutes)
+                _dns = address.dns_settings
+                self.handle_changed_property('domain_name_label',
+                                              _dns and _dns.domain_name_label)
+                self.handle_changed_property('reverse_fqdn',
+                                              _dns and _dns.reverse_fqdn)
             else:
                 self.warn_not_supposed_to_exist(valuable_resource = True)
                 self.confirm_destroy()
@@ -105,13 +142,19 @@ class AzureReservedIPAddressState(ResourceState):
                                 "please run 'deploy --check' to fix this")
 
             self.log("creating {0} in {1}...".format(self.full_name, defn.location))
-            req = self.sms().create_reserved_ip_address(defn.reserved_ip_address_name,
-                                                        label = defn.label,
-                                                        location = defn.location)
-            self.finish_request(req)
+            self._update_resource(defn)
+            self.log("reserved IP address: {0}".format(self.ip_address))
 
-            address = self.get_settled_resource()
-            self.state = self.UP
-            self.copy_properties(defn)
-            self.ip_address = address.address
-            self.azure_id = address.id
+        if self.properties_changed(defn):
+            self.log("updating properties of {0}...".format(self.full_name))
+            if not self.get_settled_resource():
+                raise Exception("{0} has been deleted behind our back; "
+                                "please run 'deploy --check' to fix this"
+                                .format(self.full_name))
+            self._update_resource(defn)
+
+
+    def create_after(self, resources, defn):
+        from nixops.resources.azure_resource_group import AzureResourceGroupState
+        return {r for r in resources
+                  if isinstance(r, AzureResourceGroupState)}
