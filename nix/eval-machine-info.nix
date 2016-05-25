8e88c86065534469f25c808a958b3f14f5e95939
Rob Vermaas <rob.vermaas@gmail.com>
2016-05-25 11:22:14 +0000

    Azure: update image to 16.03

--- nix/eval-machine-info.nix
+++ nix/eval-machine-info.nix
@@ -210,7 +210,7 @@ rec {
         blobType = "PageBlob";
         copyFromBlob = if args ? azure-image-url
                        then args.azure-image-url
-                       else "https://nixos.blob.core.windows.net/images/nixos-unstable-nixops.vhd";
+                       else "https://nixos.blob.core.windows.net/images/nixos-image-16.03.847.8688c17-x86_64-linux.vhd";
       })]
     )
   ) azure_deployments;
