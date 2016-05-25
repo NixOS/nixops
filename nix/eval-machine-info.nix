75278944dfe756c656a5079a2eeafee4f6059498
Rob Vermaas <rob.vermaas@gmail.com>
2016-05-25 10:21:56 +0000

    GCE: add 16.03 image

--- nix/eval-machine-info.nix
+++ nix/eval-machine-info.nix
@@ -229,15 +229,17 @@ rec {
                       ( n: v: let dc = (scrubOptionValue v).config.deployment; in dc.targetEnv == "gce" );
 
   gce_default_bootstrap_images = flip mapAttrs' gce_deployments (name: depl:
-    let gce = (scrubOptionValue depl).config.deployment.gce; in (
+    let
+      gce = (scrubOptionValue depl).config.deployment.gce; 
+      images = {
+        "14.12" = "gs://nixos-cloud-images/nixos-14.12.471.1f09b77-x86_64-linux.raw.tar.gz";
+        "15.09" = "gs://nixos-cloud-images/nixos-15.09.425.7870f20-x86_64-linux.raw.tar.gz";
+        "16.03" = "gs://nixos-cloud-images/nixos-image-16.03.847.8688c17-x86_64-linux.raw.tar.gz";
+      };
+    in (
       nameValuePair ("bootstrap") [({ pkgs, ...}: {
         inherit (gce) project serviceAccount accessKey;
-        sourceUri =
-          if pkgs.lib.substring 0 5 pkgs.lib.nixpkgsVersion == "14.12" then
-            "gs://nixos-cloud-images/nixos-14.12.471.1f09b77-x86_64-linux.raw.tar.gz"
-          else
-            "gs://nixos-cloud-images/nixos-15.09.425.7870f20-x86_64-linux.raw.tar.gz";
-
+        sourceUri = images."${pkgs.lib.substring 0 5 pkgs.lib.nixpkgsVersion}" or images."15.09";
       })]
     )
   );
