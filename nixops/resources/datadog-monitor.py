f17ac4b9212aa51834c9b65c5e51ccd88aae7d9f
Amine Chikhaoui <amine.chikhaoui91@gmail.com>
2016-08-17 13:29:21 +0100

    style

--- nixops/resources/datadog-monitor.py
+++ nixops/resources/datadog-monitor.py
@@ -19,12 +19,12 @@ class DatadogMonitorDefinition(nixops.resources.ResourceDefinition):
     def __init__(self, xml):
         nixops.resources.ResourceDefinition.__init__(self, xml)
         self.thresholds = {}
-        self.monitor_name = xml.find("attrs/attr[@name='name']/string").get("value")
-        self.monitor_type = xml.find("attrs/attr[@name='type']/string").get("value")
-        self.monitor_query = xml.find("attrs/attr[@name='query']/string").get("value")
+        self.monitorName = xml.find("attrs/attr[@name='name']/string").get("value")
+        self.monitorType = xml.find("attrs/attr[@name='type']/string").get("value")
+        self.monitorQuery = xml.find("attrs/attr[@name='query']/string").get("value")
         self.api_key = xml.find("attrs/attr[@name='api_key']/string").get("value")
         self.app_key = xml.find("attrs/attr[@name='app_key']/string").get("value")
-        self.monitor_message =  xml.find("attrs/attr[@name='message']/string").get("value")
+        self.monitorMessage =  xml.find("attrs/attr[@name='message']/string").get("value")
         for alert in xml.findall("attrs/attr[@name='thresholds']/attrs/attr"):
             if alert.attrib.get('name') == "ok":
                 self.thresholds['ok'] = int(alert.find("int").get("value"))
@@ -42,11 +42,11 @@ class DatadogMonitorState(nixops.resources.ResourceState):
     state = nixops.util.attr_property("state", nixops.resources.ResourceState.MISSING, int)
     api_key = nixops.util.attr_property("api_key", None)
     app_key = nixops.util.attr_property("app_key", None)
-    monitor_name = nixops.util.attr_property("monitor_name", None)
-    monitor_type = nixops.util.attr_property("monitor_type", None)
-    monitor_query = nixops.util.attr_property("monitor_query", None)
-    monitor_message = nixops.util.attr_property("monitor_message", None)
-    monitor_id = nixops.util.attr_property("monitor_id", None)
+    monitorName = nixops.util.attr_property("monitor_name", None)
+    monitorType = nixops.util.attr_property("monitor_type", None)
+    monitorQuery = nixops.util.attr_property("monitor_query", None)
+    monitorMessage = nixops.util.attr_property("monitor_message", None)
+    monitorId = nixops.util.attr_property("monitor_id", None)
 
     @classmethod
     def get_type(cls):
@@ -56,7 +56,7 @@ class DatadogMonitorState(nixops.resources.ResourceState):
     def __init__(self, depl, name, id):
         nixops.resources.ResourceState.__init__(self, depl, name, id)
         self._dd_api = None
-        self._key_options = None
+        self._keyOptions = None
 
     def _exists(self):
         return self.state != self.MISSING
@@ -68,7 +68,7 @@ class DatadogMonitorState(nixops.resources.ResourceState):
 
     @property
     def resource_id(self):
-        return self.monitor_name
+        return self.monitorName
 
 
     def get_definition_prefix(self):
@@ -76,19 +76,19 @@ class DatadogMonitorState(nixops.resources.ResourceState):
 
     def connect(self, app_key, api_key):
         if self._dd_api: return
-        self._dd_api, self._key_options = nixops.datadog_utils.initializeDatadog(app_key=app_key, api_key=api_key)
+        self._dd_api, self._keyOptions = nixops.datadog_utils.initializeDatadog(app_key=app_key, api_key=api_key)
 
     def create(self, defn, check, allow_reboot, allow_recreate):
         if check or self.state != self.UP:
             self.connect(app_key=defn.app_key, api_key=defn.api_key)
-            if self.monitor_id != None:
-                self._dd_api.Monitor.delete(self.monitor_id)
-            self.log("creating Datadog monitor '{0}...'".format(defn.monitor_name))
-            options = self._key_options
+            if self.monitorId != None:
+                self._dd_api.Monitor.delete(self.monitorId)
+            self.log("creating Datadog monitor '{0}...'".format(defn.monitorName))
+            options = self._keyOptions
             if defn.thresholds != {}: options.update(defn.thresholds)
             response = self._dd_api.Monitor.create(
-                type=defn.monitor_type, query=defn.monitor_query, name=defn.monitor_name,
-                message=defn.monitor_message, options=options)
+                type=defn.monitorType, query=defn.monitorQuery, name=defn.monitorName,
+                message=defn.monitorMessage, options=options)
             if 'errors' in response:
                 raise Exception(str(response['errors']))
             else:
@@ -98,19 +98,19 @@ class DatadogMonitorState(nixops.resources.ResourceState):
             self.state = self.UP
             self.api_key = defn.api_key
             self.app_key = defn.app_key
-            self.monitor_name = defn.monitor_name
-            self.monitor_type = defn.monitor_type
-            self.monitor_query = defn.monitor_query
-            self.monitor_message = defn.monitor_message
-            self.monitor_id = monitorId
+            self.monitorName = defn.monitorName
+            self.monitorType = defn.monitorType
+            self.monitorQuery = defn.monitorQuery
+            self.monitorMessage = defn.monitorMessage
+            self.monitorId = monitorId
 
 
 
 
     def destroy(self, wipe=False):
         if self.state == self.UP:
-            self.log("deleting Datadog monitor ‘{0}’...".format(self.monitor_name))
+            self.log("deleting Datadog monitor ‘{0}’...".format(self.monitorName))
             self.connect(self.app_key,self.api_key)
-            self._dd_api.Monitor.delete(self.monitor_id)
+            self._dd_api.Monitor.delete(self.monitorId)
 
         return True
