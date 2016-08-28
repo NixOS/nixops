7b9cef0b5702e5bcf11b2a1cf81cfeefcf8e72b7
Eric Sagnes <eric.sagnes@gmail.com>
2016-08-29 00:47:21 +0900

    Update image build script

--- maintainers/build-virtualbox-image.sh
+++ maintainers/build-virtualbox-image.sh
@@ -2,8 +2,10 @@
 export NIXOS_CONFIG=$(readlink -f $(dirname $0)/..)/nix/virtualbox-image-nixops.nix
 version=$(nix-instantiate --eval-only '<nixpkgs/nixos>' -A config.system.nixosVersion | sed s/'"'//g)
 echo "version = $version"
-nix-build '<nixpkgs/nixos>' -A config.system.build.virtualBoxImage
-name=virtualbox-nixops-$version.vdi.xz
-xz < ./result/disk.vdi > $name
+nix-build '<nixpkgs/nixos>' -A config.system.build.virtualBoxOVA
+mkdir ova && tar -xf ./result/*.ova -C ova && mv ova/{nixos*,nixos}.vmdk
+name=virtualbox-nixops-$version.vmdk.xz
+xz < ./ova/nixos.vmdk > $name
+rm -fr ova
 scp -p $name hydra-mirror@nixos.org:/data/releases/nixos/virtualbox-nixops-images/
 sha256sum $name
