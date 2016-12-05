c8f4b2dfa37fe37e87c0a73f1c47dc9fb7f82a11
Tom Hunger <tehunger@gmail.com>
2016-12-05 20:58:51 +0000

    Tighting a bit.

--- examples/trivial-digital-ocean.nix
+++ examples/trivial-digital-ocean.nix
@@ -1,19 +1,13 @@
 {
+  resources.sshKeyPairs.ssh-key = {};
+
   machine = { config, pkgs, ... }: {
+    services.nginx.enable = true;
+    networking.firewall.allowPing = true;
+    services.openssh.enable = true;
 
-  environment.systemPackages = [ pkgs.git ];
-  services.nginx.enable = true;
-
-  networking.useDHCP = true;
-  networking.firewall.allowPing = true;
-  networking.firewall.enable = false;
-  services.openssh.enable = true;
-
-  security.grsecurity.enable = true;
-
-  deployment.targetEnv = "digital-ocean";
-  deployment.digital-ocean.region = "ams2";
-  deployment.digital-ocean.size = "512mb";
-  deployment.digital-ocean.keyName = "teh";
+    deployment.targetEnv = "digital-ocean";
+    deployment.digital-ocean.region = "ams2";
+    deployment.digital-ocean.size = "512mb";
   };
 }
--- nixops/backends/digital_ocean.py
+++ nixops/backends/digital_ocean.py
@@ -1,24 +1,35 @@
 # -*- coding: utf-8 -*-
+"""
+A backend for www.digitalocean.com (short as "DO").
+
+This backend uses nixos-infect (which uses nixos LUSTRATE) to infect a
+Ubuntu digitial ocean instance. The setup requires two reboots, one for
+the infect itself, another after we pushed the nixos image.
+
+I hit a few subtle problems along the way:
+* DO doesn't do dhcp so we have to hard-code the network configuration
+* Ubuntu still uses eth0, 1 etc, not enp0s3 etc so we have a network
+  link name change after the reboot.
+* I had to modify nixos-infect to reflect the network link name changes,
+  and to not reboot to avoid ssh-interruption and therefore errors.
+
+Still to do:
+* Floating IPs
+* Network attached storage
+"""
 import os
 import os.path
-import sys
-import re
 import time
-import math
-import shutil
 import nixops.resources
 from nixops.backends import MachineDefinition, MachineState
-from nixops.nix_expr import Function, Call, RawValue
+from nixops.nix_expr import Function, RawValue
 import nixops.util
 import nixops.known_hosts
-from xml import etree
-import datetime
-import digitalocean
 import socket
-
-SSH_KEY = """ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAtq8LpgrnFQWpIcK5YdrQNzu22sPrbkHKD83g8v/s7Nu3Omb7h5TLBOZ6DYPSorGMKGjDFqo0witXRagWq95HaA9epFXmhJlO3NTxyTAzIZSzql+oJkqszNpmYY09L00EIplE/YKXPlY2a+sGx3CdJxbglGfTcqf0J2DW4wO2ikZSOXRiLEbztyDwc+TNwYJ3WtzTFWhG/9hbbHGZtpwQl6X5l5d2Mhl2tlKJ/zQYWV1CVXLSyKhkb4cQPkL05enguCQgijuI/WsUE6pqdl4ypziXGjlHAfH+zO06s6EDMQYr50xgYRuCBicF86GF8/fOuDJS5CJ8/FWr16fiWLa2Aw== tom@leto"""
+import digitalocean
 
 infect_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data', 'nixos-infect'))
+
 
 class DigitalOceanDefinition(MachineDefinition, nixops.resources.ResourceDefinition):
     @classmethod
@@ -30,7 +41,6 @@ class DigitalOceanDefinition(MachineDefinition, nixops.resources.ResourceDefinit
         self.auth_token = config["digital-ocean"]["authToken"]
         self.region = config["digital-ocean"]["region"]
         self.size = config["digital-ocean"]["size"]
-        self.key_name = config["digital-ocean"]["keyName"]
 
     def show_type(self):
         return "{0} [{1}]".format(self.get_type(), self.region)
@@ -48,19 +58,24 @@ class DigitalOceanState(MachineState):
     netmask = nixops.util.attr_property("netmask", None)
     region = nixops.util.attr_property("digital-ocean.region", None)
     size = nixops.util.attr_property("digital-ocean.size", None)
-    key_name = nixops.util.attr_property("digital-ocean.keyName", None)
     auth_token = nixops.util.attr_property("digital-ocean.authToken", None)
     droplet_id = nixops.util.attr_property("digital-ocean.dropletId", None)
+    key_pair = nixops.util.attr_property("digital-ocean.keyPair", None)
 
     def __init__(self, depl, name, id):
         MachineState.__init__(self, depl, name, id)
+        self.name = name
 
     def get_ssh_name(self):
         return self.public_ipv4
 
     def get_ssh_flags(self, *args, **kwargs):
         super_flags = super(DigitalOceanState, self).get_ssh_flags(*args, **kwargs)
-        return super_flags + ['-o', 'UserKnownHostsFile=/dev/null', '-o', 'StrictHostKeyChecking=no']
+        return super_flags + [
+            '-o', 'UserKnownHostsFile=/dev/null',
+            '-o', 'StrictHostKeyChecking=no',
+            '-i', self.get_ssh_private_key_file(),
+        ]
 
     def get_physical_spec(self):
         prefixLength = bin(int(socket.inet_aton(self.netmask).encode('hex'), 16)).count('1')
@@ -76,17 +91,21 @@ class DigitalOceanState(MachineState):
             },
             ('boot', 'loader', 'grub', 'device'): 'nodev', # keep ubuntu bootloader?
             ('fileSystems', '/'): { 'device': '/dev/vda1', 'fsType': 'ext4'},
-            ('users', 'extraUsers', 'root', 'openssh', 'authorizedKeys', 'keys'): [SSH_KEY],
+            ('users', 'extraUsers', 'root', 'openssh', 'authorizedKeys', 'keys'): [self.depl.active_resources.get('ssh-key').public_key],
         })
 
     def get_ssh_private_key_file(self):
-        return open('/home/tom/.ssh/id_rsa').read()
+        return self.write_ssh_private_key(self.depl.active_resources.get('ssh-key').private_key)
 
     def create_after(self, resources, defn):
-        return set()
+        # make sure the ssh key exists before we do anything else
+        return {
+            r for r in resources if
+            isinstance(r, nixops.resources.ssh_keypair.SSHKeyPairState)
+        }
 
     def get_auth_token(self):
-        return os.environ.get('DIGITAL_OCEAN_AUTH_TOKEN')
+        return os.environ.get('DIGITAL_OCEAN_AUTH_TOKEN', self.auth_token)
 
     def destroy(self, wipe=False):
         self.log("destroying droplet {}".format(self.droplet_id))
@@ -99,20 +118,24 @@ class DigitalOceanState(MachineState):
         self.droplet_id = None
 
     def create(self, defn, check, allow_reboot, allow_recreate):
+        ssh_key = self.depl.active_resources.get('ssh-key')
+        if ssh_key is None:
+            raise Exception('Please specify a ssh-key resource (resources.sshKeyPairs.ssh-key = {}).')
+
         if self.droplet_id is not None:
             return
 
-        """Create or update the resource defined by ‘defn’."""
         self.manager = digitalocean.Manager(token=self.get_auth_token())
         droplet = digitalocean.Droplet(
             token=self.get_auth_token(),
-            name='Example',
+            name=self.name,
             region=defn.region,
-            ssh_keys=[SSH_KEY],
+            ssh_keys=[ssh_key.public_key],
             image='ubuntu-16-04-x64', # only for lustration
             size_slug=defn.size,
         )
-        self.log_start("Creating droplet ...")
+
+        self.log_start("creating droplet ...")
         droplet.create()
 
         status = 'in-progress'
@@ -132,7 +155,6 @@ class DigitalOceanState(MachineState):
         self.droplet_id = droplet.id
         self.public_ipv4 = droplet.ip_address
         self.log_end("{}".format(droplet.ip_address))
-
 
         # Not sure when I'd have more than one interface from the DO
         # API but networks is an array nevertheless.
