725a59a1e442e39e7118c86c1bbb23d1df86c854
Rob Vermaas <rob.vermaas@gmail.com>
2017-01-04 12:22:27 +0000

    Add --keep-going and --fallback to virtualbox creation script for convenience.

--- maintainers/build-virtualbox-image.sh
+++ maintainers/build-virtualbox-image.sh
@@ -2,7 +2,7 @@
 export NIXOS_CONFIG=$(readlink -f $(dirname $0)/..)/nix/virtualbox-image-nixops.nix
 version=$(nix-instantiate --eval-only '<nixpkgs/nixos>' -A config.system.nixosVersion | sed s/'"'//g)
 echo "version = $version"
-nix-build '<nixpkgs/nixos>' -A config.system.build.virtualBoxOVA
+nix-build '<nixpkgs/nixos>' -A config.system.build.virtualBoxOVA --keep-going --fallback
 mkdir ova && tar -xf ./result/*.ova -C ova && mv ova/{nixos*,nixos}.vmdk
 name=virtualbox-nixops-$version.vmdk.xz
 xz < ./ova/nixos.vmdk > $name
