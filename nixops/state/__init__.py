6da031f332ea229903be11bec085788e8fb7f196
Maarten Hoogendoorn <maarten@moretea.nl>
2017-03-13 20:19:01 +0100

    Introduce different state state schemes.
    Needed to faclitate multiple backends (like sqlite3/other remote database)

---
+++ nixops/state/__init__.py
@@ -0,0 +1,23 @@
+import urlparse
+import sys
+import file
+
+class WrongStateSchemeException(Exception):
+    pass
+
+def open(url):
+    url = urlparse.urlparse(url)
+    scheme = url.scheme
+
+    if scheme == "":
+        scheme = "file"
+
+    def raise_(ex):
+        raise ex
+
+    switcher = {
+            "file": lambda(url): file.StateFile(url.path),
+    }
+
+    function = switcher.get(scheme, lambda(url): raise_(WrongStateSchemeException("Unknown state scheme!")))
+    return function(url)
