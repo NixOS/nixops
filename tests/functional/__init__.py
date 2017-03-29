4aaf084d0459d4ad61f7cc5455beefa72de9902b
Maarten Hoogendoorn <maarten@moretea.nl>
2017-03-29 13:35:50 +0200

    Rename file -> sqlite3_file in tests

--- tests/functional/__init__.py
+++ tests/functional/__init__.py
@@ -1,6 +1,6 @@
 import os
 from os import path
-import nixops.state.file
+import nixops.state.sqlite3_file
 from tests import db_file
 
 
@@ -8,7 +8,7 @@ class DatabaseUsingTest(object):
     _multiprocess_can_split_ = True
 
     def setup(self):
-        self.sf = nixops.state.file.StateFile(db_file)
+        self.sf = nixops.state.sqlite3_file.StateFile(db_file)
 
     def teardown(self):
         self.sf.close()
