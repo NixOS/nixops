c28a4f7acedbc1b3b8446f3713f219109da19134
Rob Vermaas <rob.vermaas@gmail.com>
2017-05-02 13:26:25 +0000

    Add 17.03 image for GCE.

--- nix/eval-machine-info.nix
+++ nix/eval-machine-info.nix
@@ -246,6 +246,7 @@ rec {
         "14.12" = "gs://nixos-cloud-images/nixos-14.12.471.1f09b77-x86_64-linux.raw.tar.gz";
         "15.09" = "gs://nixos-cloud-images/nixos-15.09.425.7870f20-x86_64-linux.raw.tar.gz";
         "16.03" = "gs://nixos-cloud-images/nixos-image-16.03.847.8688c17-x86_64-linux.raw.tar.gz";
+        "17.03" = "gs://nixos-cloud-images/nixos-image-17.03.1082.4aab5c5798-x86_64-linux.raw.tar.gz";
       };
     in (
       nameValuePair ("bootstrap") [({ pkgs, ...}: {
