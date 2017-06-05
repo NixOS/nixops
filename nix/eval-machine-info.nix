6b77a54daf47a8dfb10841596fe259de8eb0759c
Amine Chikhaoui <amine@chikhaoui.tn>
2017-06-05 11:19:36 +0100

    Add vpc network ACLs to the top level eval-machine-info

--- nix/eval-machine-info.nix
+++ nix/eval-machine-info.nix
@@ -104,6 +104,7 @@ rec {
   resources.vpcSubnets = evalResources ./vpc-subnet.nix (zipAttrs resourcesByType.vpcSubnets or []);
   resources.vpcInternetGateways = evalResources ./vpc-internet-gateway.nix (zipAttrs resourcesByType.vpcInternetGateways or []);
   resources.vpcDhcpOptions = evalResources ./vpc-dhcp-options.nix (zipAttrs resourcesByType.vpcDhcpOptions or []);
+  resources.vpcNetworkAcls = evalResources ./vpc-network-acl.nix (zipAttrs resourcesByType.vpcNetworkAcls or []);
   resources.machines = mapAttrs (n: v: v.config) nodes;
 
   # Datadog resources
