829c9dcb4cab53c54a5a5ec9a029d79ae10b3521
Amine Chikhaoui <amine@chikhaoui.tn>
2017-06-21 16:17:26 +0100

    adding vpc nat gateway options

---
+++ nix/vpc-nat-gateway.nix
@@ -0,0 +1,40 @@
+{ config, lib, uuid, name, ... }:
+
+with lib;
+
+{
+  options = {
+
+    name = mkOption {
+      default = "charon-${uuid}-${name}";
+      type = types.str;
+      description = "Name of the VPC NAT gateway.";
+    };
+
+    accessKeyId = mkOption {
+      type = types.str;
+      description = "The AWS Access Key ID.";
+    };
+
+    region = mkOption {
+      type = types.str;
+      description = "AWS region.";
+    };
+
+    allocationId = mkOption {
+      type = types.either types.str (resource "elastic-ip");
+      apply = x: if builtins.isString x then x else "res-" + x._name + "." + x._type + "allocation_id"";
+      description = ''
+        The allocation ID of the elastic IP address.
+      '';
+    };
+
+    subnetId = mkOption {
+      type = types.either types.str (resource "vpc-subnet");
+      apply = x: if builtins.isString x then x else "res-" + x._name;
+      description = ''
+        The ID of the VPC subnet where the NAT gateway will be created
+      '';
+    };
+  } // import ./common-ec2-options.nix { inherit lib; };
+}
