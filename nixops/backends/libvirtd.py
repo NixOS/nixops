NixOS/nixops

commit e4105635c18fc37b175438f7e9a0c41a6d419dab

libvirtd: create "persistent" instead of "transient" domains
Persistent domains/VMs are created by "virsh define domain.xml" whereas
transient domains are created (and started) with "virsh create domain.xml".

This ensures that nixops deployments/VMs survive a reboot(!).

See
https://wiki.libvirt.org/page/VM_lifecycle#Transient_guest_domains_vs_Persistent_guest_domains

nixops/backends/libvirtd.py
@@ -106,6 +106,11 @@ class LibvirtdState(MachineState):
             # TODO: use libvirtd.extraConfig to make the image accessible for your user
             os.chmod(self.disk_path, 0666)
             self.vm_id = self._vm_id()
+            dom_file = self.depl.tempdir + "/{0}-domain.xml".format(self.name)
+            nixops.util.write_file(dom_file, self.domain_xml)
+            # By using "virsh define" we ensure that the domain is
+            # "persistent", as opposed to "transient" (removed on reboot).
+            self._logged_exec(["virsh", "-c", "qemu:///system", "define", dom_file])
         self.start()
         return True
 
@@ -206,9 +211,7 @@ class LibvirtdState(MachineState):
             self.private_ipv4 = self._parse_ip()
         else:
             self.log("starting...")
-            dom_file = self.depl.tempdir + "/{0}-domain.xml".format(self.name)
-            nixops.util.write_file(dom_file, self.domain_xml)
-            self._logged_exec(["virsh", "-c", "qemu:///system", "create", dom_file])
+            self._logged_exec(["virsh", "-c", "qemu:///system", "start", self.vm_id])
             self._wait_for_ip(0)
 
     def get_ssh_name(self):

commit 4b3b8cd05badd18a76fe73d307b116fcde8b2ba6

libvirt: undefine domain on "nixops destroy"
This fixes a regression introduced in commit e4105635c18f ("libvirtd:
create "persistent" instead of "transient" domains"). Now that domains
are persistent, we must de-allocate resources with "virsh undefine".

nixops/backends/libvirtd.py
@@ -231,6 +231,7 @@ class LibvirtdState(MachineState):
             return True
         self.log_start("destroying... ")
         self.stop()
+        self._logged_exec(["virsh", "-c", "qemu:///system", "undefine", self.vm_id])
         if (self.disk_path and os.path.exists(self.disk_path)):
             os.unlink(self.disk_path)
         return True