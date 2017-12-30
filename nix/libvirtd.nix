990e39e704953ca49e26126a2760735639749df0
Emmanuel Rosa <goaway1000@gmail.com>
2017-12-29 21:11:40 -0500

    libvirtd: fix mknod error
    This changeset modifies the libvirtd image Nix expressions to only call `mknod` when the block device doesn't already exist, therefore avoiding a build failure.
    
    ref #827

--- nix/libvirtd.nix
+++ nix/libvirtd.nix
@@ -22,8 +22,12 @@ let
           '';
       }
       ''
-        . /sys/class/block/vda1/uevent
-        mknod /dev/vda1 b $MAJOR $MINOR
+        if [ ! -b /dev/vda1 ] 
+        then
+            . /sys/class/block/vda1/uevent
+            mknod /dev/vda1 b $MAJOR $MINOR
+        fi
+
         mkdir /mnt
         mount /dev/vda1 /mnt
 
