1f9505a37b738950fb059589d177545968cd07c0
Rob Vermaas <rob.vermaas@gmail.com>
2018-01-09 12:39:35 +0100

    route53: add example

---
+++ examples/route53.nix
@@ -0,0 +1,54 @@
+
+{
+  resources.route53HostedZones.hs =
+      { name = "nixos.org.";
+        comment = "Hosted zone for nixos.org";
+      };
+
+  resources.route53RecordSets = {
+    
+    a-record = { resources, ... }: {
+      zoneId = resources.route53HostedZones.hs;
+      domainName = "www.nixos.org.";
+      ttl = 300;
+      recordValues = [ "1.2.3.4" ];
+      recordType = "A";
+    };
+
+    mv1 = { resources, ... }: {
+       zoneId = resources.route53HostedZones.hs;
+       domainName = "mv.nixos.org.";
+       recordValues = [ "1.2.3.4" ];
+       recordType = "A";
+       setIdentifier = "id1";
+     };
+
+
+     mv2 = { resources, ... }: {
+       zoneId = resources.route53HostedZones.hs;
+       domainName = "mv.nixos.org.";
+       ttl = 300;
+       recordValues = [ "4.3.2.1" ];
+       recordType = "A";
+       setIdentifier = "id2";
+     };
+
+     weight1 = { resources, ... }: {
+        zoneId = resources.route53HostedZones.hs;
+        domainName = "weight.nixos.org.";
+        weight = 10;
+        recordValues = [ "5.4.3.2" ];
+        recordType = "A";
+        setIdentifier = "id1";
+      };
+
+      weight2 = { resources, ... }: {
+        zoneId = resources.route53HostedZones.hs;
+        domainName = "weight.nixos.org.";
+        weight = 20;
+        recordValues = [ "2.3.4.5" ];
+        recordType = "A";
+        setIdentifier = "id2";
+      };
+ };
+}
