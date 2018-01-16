9e75ff76cc40081fc901481a823f6758ad5e1857
Rob Vermaas <rob.vermaas@gmail.com>
2018-01-16 12:13:12 +0100

    digitalocean: allow hard reboot (via API, using --hard)

--- nixops/backends/digital_ocean.py
+++ nixops/backends/digital_ocean.py
@@ -193,3 +193,14 @@ class DigitalOceanState(MachineState):
         self.log_start("running nixos-infect")
         self.run_command('bash </dev/stdin 2>&1', stdin=open(infect_path))
         self.reboot_sync()
+
+    def reboot(self, hard=False):
+        if hard:
+            self.log("sending hard reset to droplet...")
+            droplet = digitalocean.Droplet(id=self.droplet_id, token=self.get_auth_token())
+            droplet.reboot()
+            self.wait_for_ssh()
+            self.state = self.STARTING
+        else:
+            MachineState.reboot(self, hard=hard)
+
