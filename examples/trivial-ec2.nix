b1ea71d70a673ef0d8d4d624ec5f6b761f487882
Rob Vermaas <rob.vermaas@gmail.com>
2018-01-22 11:54:29 +0100

    Use t2 as default trivial ec2 example, as m1 is not supported anymore.

--- examples/trivial-ec2.nix
+++ examples/trivial-ec2.nix
@@ -3,6 +3,6 @@
     { imports = [ ./ec2-info.nix ];
       deployment.targetEnv = "ec2";
       deployment.ec2.region = "us-east-1";
-      deployment.ec2.instanceType = "m1.small";
+      deployment.ec2.instanceType = "t2.medium";
     };
 }
