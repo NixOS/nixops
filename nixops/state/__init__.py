28ac4b2357b0aa667631c77e45eb23d4984cfad6
Matthew O'Gorman <mog@rldn.net>
2018-02-13 14:21:18 -0500

    move function into the core state file

--- nixops/state/__init__.py
+++ nixops/state/__init__.py
@@ -8,6 +8,21 @@ import sqlite_connector
 class WrongStateSchemeException(Exception):
     pass
 
+
+def get_default_state_file():
+    home = os.environ.get("HOME", "") + "/.nixops"
+    if not os.path.exists(home):
+        old_home = os.environ.get("HOME", "") + "/.charon"
+        if os.path.exists(old_home):
+            sys.stderr.write("renaming {!r} to {!r}...\n".format(old_home, home))
+            os.rename(old_home, home)
+            if os.path.exists(home + "/deployments.charon"):
+                os.rename(home + "/deployments.charon", home + "/deployments.nixops")
+        else:
+            os.makedirs(home, 0700)
+    return os.environ.get("NIXOPS_STATE", os.environ.get("CHARON_STATE", home + "/deployments.nixops"))
+
+
 def open(url):
     print 'url = {}'.format(url)
     url_parsed = urlparse.urlparse(url)
