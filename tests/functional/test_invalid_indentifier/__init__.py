5f4cd5de081169ad682a51aa138da63c6423265f
srghma <srghma@gmail.com>
2018-06-16 14:10:16 +0300

    fix: tests

--- tests/functional/test_invalid_indentifier/__init__.py
+++ tests/functional/test_invalid_indentifier/__init__.py
@@ -1,15 +1,19 @@
 from nose import tools
 from itertools import product
+from os import path
 from parameterized import parameterized
 
 from tests.functional.shared.deployment_run_command import deployment_run_command
 from tests.functional.shared.create_deployment import create_deployment
-from tests.functional.shared.using_state_file import using_state_file
+from tests.functional.shared.using_unique_state_file import using_unique_state_file
 
 parent_dir = path.dirname(__file__)
 
 @parameterized(product(
-    ['json', 'nixops'],
+    [
+        'json',
+        'nixops'
+    ],
     [
         [
             '{}/invalid-identifier.nix'.format(parent_dir),
@@ -17,7 +21,10 @@ parent_dir = path.dirname(__file__)
     ]
 ))
 def test_invalid_indentifier(state_extension, nix_expressions):
-    with using_state_file(state_extension) as state:
+    with using_unique_state_file(
+            [test_invalid_indentifier.__name__],
+            state_extension
+        ) as state:
         deployment = create_deployment(state, nix_expressions)
         with tools.assert_raises(Exception):
             deployment.evaluate()
--- tests/functional/test_query_deployments/__init__.py
+++ tests/functional/test_query_deployments/__init__.py
@@ -3,13 +3,20 @@ from parameterized import parameterized
 
 from tests.functional.shared.deployment_run_command import deployment_run_command
 from tests.functional.shared.create_deployment import create_deployment
-from tests.functional.shared.using_state_file import using_state_file
+from tests.functional.shared.using_unique_state_file import using_unique_state_file
 
 @parameterized(
-    ['json', 'nixops']
+    [
+        'json',
+        'nixops'
+    ]
 )
 def test_query_deployments(state_extension):
-    with using_state_file(state_extension) as state:
+    with using_unique_state_file(
+            [test_query_deployments.__name__],
+            state_extension
+        ) as state:
+
         deployments = []
 
         for i in range(10):
