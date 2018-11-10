b5ee9152401848c7cf050891d6aaa8c88840ccb8
Sergei Khoma <srghma@gmail.com>
2018-11-10 20:35:55 +0200

    refactor: coretemp review -> fix typo -> "michine" to "machine"

--- tests/functional/shared/deployment_run_command.py
+++ tests/functional/shared/deployment_run_command.py
@@ -1,8 +1,8 @@
 from nixops.util import ansi_highlight
 
-def deployment_run_command(deployment, command, michine_index=0):
+def deployment_run_command(deployment, command, machine_index=0):
     deployment.evaluate()
-    machine = deployment.machines.values()[michine_index]
+    machine = deployment.machines.values()[machine_index]
 
     debug_message = ansi_highlight('tests> ') + command
     import sys; print >> sys.__stdout__, debug_message
