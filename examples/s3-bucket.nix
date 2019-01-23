c074196254bd649728d5edc819d9789ae38f8ca5
AmineChikhaoui <amine.chikhaoui91@gmail.com>
2019-01-23 14:50:41 -0500

    minor style fixes

--- examples/s3-bucket.nix
+++ examples/s3-bucket.nix
@@ -3,44 +3,44 @@
 , ...
 }:
 {
-resources.s3Buckets.s3 = 
-  { 
-    inherit region accessKeyId; 
-    name = "s3-test-bucket";
-    policy = ''
-      {
-        "Version": "2012-10-17",
-        "Statement": [
-          {
-            "Sid": "testing",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": "s3:GetObject",
-            "Resource": "arn:aws:s3:::s3-test-bucket/*"
-          }
-        ]
-      }
-      '';
-     lifeCycle = ''
-       {
-         "Rules": [
-           {
-             "Status": "Enabled", 
-             "Prefix": "", 
-             "Transitions": [
-                {
-                  "Days": 30,
-                  "StorageClass": "GLACIER"
-                }
-             ], 
-             "ID": "Glacier",
-            "AbortIncompleteMultipartUpload": 
-               {
-                  "DaysAfterInitiation": 7
+  resources.s3Buckets.s3-test-bucket =
+    {
+      inherit region accessKeyId;
+      name = "s3-test-bucket";
+      policy = ''
+        {
+          "Version": "2012-10-17",
+          "Statement": [
+            {
+              "Sid": "testing",
+              "Effect": "Allow",
+              "Principal": "*",
+              "Action": "s3:GetObject",
+              "Resource": "arn:aws:s3:::s3-test-bucket/*"
+            }
+          ]
+        }
+        '';
+       lifeCycle = ''
+         {
+           "Rules": [
+              {
+                "Status": "Enabled",
+                "Prefix": "",
+                "Transitions": [
+                  {
+                    "Days": 30,
+                    "StorageClass": "GLACIER"
+                  }
+                ],
+                "ID": "Glacier",
+                "AbortIncompleteMultipartUpload":
+                  {
+                    "DaysAfterInitiation": 7
+                  }
               }
-           }
-         ]
-       }
-     ''; 
-  };
+           ]
+         }
+       '';
+    };
 }
