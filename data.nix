baa765bac5c5d54baae6ff61013ed3699f02a2a8
John Lotoski <john.lotoski@iohk.io>
2019-09-04 13:39:59 -0400

    Update the aws and hetzner repos to the nixos github account

--- data.nix
+++ data.nix
@@ -2,16 +2,16 @@
 {
   aws =
     {
-      owner   = "input-output-hk";
+      owner   = "nixos";
       repo    = "nixops-aws";
-      version = "0.0.2";
-      sha256  = "1z1f2sk2jkyi1xcn7k6j619vs0sjd13xajvy2ybm75v29mz9sm4r";
+      version = "1.0.0";
+      sha256  = "1if6spscsgd6ckivgvbqza5fvvn5hbafi1n8q0fw98s3xpz2hjfm";
     };
   hetzner =
     {
-      owner   = "input-output-hk";
+      owner   = "nixos";
       repo    = "nixops-hetzner";
-      version = "0.0.1";
-      sha256  = "1nvdx3p7xb7klb0c9jbfwq2mf8i1hxvwvxxaabjpby5sza8s0qf5";
+      version = "1.0.0";
+      sha256  = "0cxfjpk2daczv3m7q5bsgfvd30qgmm1y7dnvz6nd7s7l7l0gsvas";
     };
 }
