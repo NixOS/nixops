3f1e40719d9382475ca48bf5186a21a9ca648b73
Domen Kožar <domen@dev.si>
2019-09-17 11:23:59 +0200

    fix manual build on nixpkgs 19.09

--- doc/manual/default.nix
+++ doc/manual/default.nix
@@ -9,24 +9,10 @@ let
                    resources = { gceImages.bootstrap = {}; };
                    name = "<name>"; uuid = "<uuid>";
                  };
-
-  options = pkgs.lib.filter (opt: opt.visible && !opt.internal)
-    (pkgs.lib.optionAttrSetToDocList systemModule.options);
-
-  backwardsCompat = { options }: let
-    optionsXML = builtins.toFile "options.xml" (builtins.unsafeDiscardStringContext
-      (builtins.toXML options));
-  in {
-    optionsDocBook = pkgs.runCommand "options-db.xml" {} ''
-      ${pkgs.libxslt.bin or pkgs.libxslt}/bin/xsltproc \
-        --stringparam revision '${revision}' \
-        --stringparam program 'nixops' \
-        -o intermediate.xml ${nixpkgs + "/nixos/doc/manual/options-to-docbook.xsl"} ${optionsXML}
-      ${pkgs.libxslt.bin or pkgs.libxslt}/bin/xsltproc \
-        -o $out ${nixpkgs + "/nixos/doc/manual/postprocess-option-descriptions.xsl"} intermediate.xml
-  ''; };
+  backwardsCompat = import ./compat.nix { inherit pkgs; };
 
 
 in (pkgs.nixosOptionsDoc or backwardsCompat) {
-  inherit options;
+  inherit (systemModule) options;
+  inherit revision;
 }
--- doc/manual/resource.nix
+++ doc/manual/resource.nix
@@ -7,17 +7,9 @@ let
   systemModule = pkgs.lib.fixMergeModules [ module ]
     { inherit pkgs; utils = {}; name = "<name>"; uuid = "<uuid>"; };
 
-  options = pkgs.lib.filter (opt: opt.visible && !opt.internal) (pkgs.lib.optionAttrSetToDocList systemModule.options);
+  backwardsCompat = import ./compat.nix { inherit pkgs; };
 
-  optionsXML = builtins.toFile "options.xml" (builtins.unsafeDiscardStringContext
-    (builtins.toXML options));
-
-  optionsDocBook = pkgs.runCommand "options-db.xml" {} ''
-    ${pkgs.libxslt.bin or pkgs.libxslt}/bin/xsltproc \
-      --stringparam revision '${revision}' \
-      -o intermediate.xml ${nixpkgs + "/nixos/doc/manual/options-to-docbook.xsl"} ${optionsXML}
-      ${pkgs.libxslt.bin or pkgs.libxslt}/bin/xsltproc \
-      -o $out ${nixpkgs + "/nixos/doc/manual/postprocess-option-descriptions.xsl"} intermediate.xml
-  '';
-
-in optionsDocBook
+in ((pkgs.nixosOptionsDoc or backwardsCompat) {
+  inherit (systemModule) options;
+  inherit revision;
+}).optionsDocBook
