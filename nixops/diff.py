641f9f2a600abf5495d6836598600dfce30938ad
Adam McCullough <merlinfmct87@gmail.com>
2020-02-29 09:51:20 -0800

    Set the default value in a saner way, add type annotation.

--- nixops/diff.py
+++ nixops/diff.py
@@ -59,7 +59,7 @@ class Diff:
         res_type: str,
     ):
         # type: (...) -> None
-        self.handlers = None
+        self.handlers: List[Handler] = []
         self._definition = config
         self._state = state
         self._depl = depl
