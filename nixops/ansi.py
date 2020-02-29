ecfda5a7567b673c0089f2a96f05002af64516bd
Graham Christensen <graham@grahamc.com>
2020-02-28 21:07:48 -0500

    ansi: make 100% typed

--- nixops/ansi.py
+++ nixops/ansi.py
@@ -1,17 +1,18 @@
 import sys
+from typing import TextIO
 
 
-def ansi_highlight(s: str, outfile=sys.stderr) -> str:
+def ansi_highlight(s: str, outfile: TextIO = sys.stderr) -> str:
     return "\033[1;35m" + s + "\033[0m" if outfile.isatty() else s
 
 
-def ansi_warn(s: str, outfile=sys.stderr) -> str:
+def ansi_warn(s: str, outfile: TextIO = sys.stderr) -> str:
     return "\033[1;33m" + s + "\033[0m" if outfile.isatty() else s
 
 
-def ansi_error(s: str, outfile=sys.stderr) -> str:
+def ansi_error(s: str, outfile: TextIO = sys.stderr) -> str:
     return "\033[1;31m" + s + "\033[0m" if outfile.isatty() else s
 
 
-def ansi_success(s: str, outfile=sys.stderr) -> str:
+def ansi_success(s: str, outfile: TextIO = sys.stderr) -> str:
     return "\033[1;32m" + s + "\033[0m" if outfile.isatty() else s
