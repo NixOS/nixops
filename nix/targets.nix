c65a96d557a8537443571c21417adad9b43ed585
Graham Christensen <graham@grahamc.com>
2020-03-06 14:37:08 -0500

    Add deploy-failed.target, which deploy-healthy.target triggers on failure
    Co-Authored-By: adisbladis <adisbladis@gmail.com>

--- nix/targets.nix
+++ nix/targets.nix
@@ -3,13 +3,22 @@
     deploy-prepare = {
       description = "Started immediately before switch-to-configuration.";
       unitConfig = {
-        Conflicts = [ "deploy-healthy.target" "deploy-complete.target" ];
+        Conflicts = [ "deploy-healthy.target" "deploy-complete.target" "deploy-failed.target" ];
         X-StopOnReconfiguration = true;
       };
     };
 
     deploy-healthy = {
       description = "Started after confirming switch-to-configuration was successful.";
+      unitConfig = {
+        OnFailure = [ "deploy-failed.target" ];
+        Conflicts = [ "deploy-prepare.target" "deploy-failed.target" ];
+        X-StopOnReconfiguration = true;
+      };
+    };
+
+    deploy-failed = {
+      description = "Started when deploy-healthy fails.";
       unitConfig = {
         Conflicts = [ "deploy-prepare.target" ];
         X-StopOnReconfiguration = true;
