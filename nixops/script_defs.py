cad96c0cbf8b2752803606dab8545e54f1282272
Graham Christensen <graham@grahamc.com>
2020-03-22 13:23:42 -0400

    add_common_{modify,deployment}_options: add types

--- nixops/script_defs.py
+++ nixops/script_defs.py
@@ -1117,7 +1117,7 @@ def add_subparser(
     return subparser
 
 
-def add_common_modify_options(subparser: ArgumentParser):
+def add_common_modify_options(subparser: ArgumentParser) -> None:
     subparser.add_argument(
         "nix_exprs",
         nargs="*",
@@ -1134,7 +1134,7 @@ def add_common_modify_options(subparser: ArgumentParser):
     )
 
 
-def add_common_deployment_options(subparser: ArgumentParser):
+def add_common_deployment_options(subparser: ArgumentParser) -> None:
     subparser.add_argument(
         "--include",
         nargs="+",
