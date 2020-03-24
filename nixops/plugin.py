fd72ee02a20cc6a37db6666f0d3a1d9f12a298f5
Graham Christensen <graham@grahamc.com>
2020-03-24 17:37:48 -0400

    LegacyBackend: init

--- nixops/plugin.py
+++ nixops/plugin.py
@@ -1,8 +1,9 @@
 from typing import Dict, Type
 import nixops.plugins
 from nixops.storage import StorageBackend
+from nixops.storage.legacy import LegacyBackend
 
 
 @nixops.plugins.hookimpl
 def register_backends() -> Dict[str, Type[StorageBackend]]:
-    return {}
+    return {"legacy": LegacyBackend}
