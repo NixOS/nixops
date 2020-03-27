434d6a0166057392f372ad6bf8f31c56899c8ecb
adisbladis <adisbladis@gmail.com>
2020-03-27 15:08:50 +0000

    Re-add manual build

--- default.nix
+++ default.nix
@@ -1,4 +1,6 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ nixpkgs ? <nixpkgs>
+, pkgs ? import nixpkgs {}
+}:
 
 let
 
@@ -18,9 +20,20 @@ in pkgs.poetry2nix.mkPoetryApplication {
     pkgs.openssh
   ];
 
+  nativeBuildInputs = [
+    pkgs.docbook5_xsl
+    pkgs.libxslt
+  ];
+
   overrides = [
     pkgs.poetry2nix.defaultPoetryOverrides
     overrides
   ];
+
+  # TODO: Manual build should be included via pyproject.toml
+  postInstall = ''
+    cp ${(import ./doc/manual { revision = "1.8"; inherit nixpkgs; }).optionsDocBook} doc/manual/machine-options.xml
+    make -C doc/manual install docdir=$out/share/doc/nixops mandir=$out/share/man
+  '';
 
 }
