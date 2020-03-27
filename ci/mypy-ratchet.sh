e3993d635da88543bcd2fd1a462bb969f626342c
adisbladis <adisbladis@gmail.com>
2020-03-27 17:25:16 +0000

    mypy-ratchet: Run first ratchet run inside a nix-shell
    We need to evaluate twice, once using any previous version of mypy and
    a second time using the current version of mypy.
    
    If we don't a mypy version bump can find new type errors that makes
    the initial check fail.

--- ci/mypy-ratchet.sh
+++ ci/mypy-ratchet.sh
@@ -1,13 +1,16 @@
-#!/usr/bin/env nix-shell
-#!nix-shell -i bash ../shell.nix
+#!/usr/bin/env bash
 
 set -eu
+
+cd "${0%/*}/.."
 
 scratch=$(mktemp -d -t tmp.XXXXXXXXXX)
 function finish {
     rm -rf "$scratch"
 }
-trap finish EXIT
+# trap finish EXIT
+
+cp ci/run-ratchet.sh $scratch/
 
 head=$(git rev-parse HEAD)
 base=origin/${GITHUB_BASE_REF:-master}
@@ -17,22 +20,12 @@ git fetch origin
 echo "Checking base branch at %s, then PR at %s...\n" "$base" "$head"
 
 git checkout "$base"
-mypy \
-    --any-exprs-report "$scratch/base" \
-    --linecount-report "$scratch/base" \
-    --lineprecision-report "$scratch/base" \
-    --txt-report "$scratch/base" \
-    nixops
+nix-shell shell.nix --run "$scratch/run-ratchet.sh $scratch base"
 
 git checkout "$head"
-mypy \
-    --any-exprs-report "$scratch/head" \
-    --linecount-report "$scratch/head" \
-    --lineprecision-report "$scratch/head" \
-    --txt-report "$scratch/head" \
-    nixops
+nix-shell shell.nix --run "$scratch/run-ratchet.sh $scratch head"
 
 diff --ignore-all-space -u100 -r  "$scratch/base/" "$scratch/head/" || true
 
-mypy ./ci/ratchet.py
-python3 ./ci/ratchet.py "$scratch"
+nix-shell shell.nix --run "mypy ./ci/ratchet.py"
+nix-shell shell.nix --run "python3 ./ci/ratchet.py $scratch"
