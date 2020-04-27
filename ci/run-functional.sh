f09e03f4348929c17e1fb52291f38192f6c89baa
adisbladis <adisbladis@gmail.com>
2020-04-27 21:19:06 +0100

    Revert testing commits
    These were never supposed to be pushed to master.

--- ci/run-functional.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env nix-shell
-#!nix-shell ../shell.nix -i bash
-set -eu
-
-# We rely on commits not in 20.03 for container testing
-export NIX_PATH=nixpkgs=https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz
-
-exec python tests.py tests/functional
