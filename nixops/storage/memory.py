fe21c5ad33e51410cb10d46951fc82379e5991e7
Graham Christensen <graham@grahamc.com>
2020-04-29 15:15:37 -0400

    fixup f52369f6b4c1cb94c7cba244ef737e814537ba3b^C

--- nixops/storage/memory.py
+++ nixops/storage/memory.py
@@ -17,17 +17,20 @@ class MemoryBackend(StorageBackend[MemoryBackendOptions]):
     def __init__(self, args: MemoryBackendOptions) -> None:
         pass
 
-    # fetchToFile: acquire a lock and download the state file to
-    # the local disk. Note: no arguments will be passed over kwargs.
-    # Making it part of the type definition allows adding new
-    # arguments later.
+    # fetchToFile: download the state file to the local disk.
+    # Note: no arguments will be passed over kwargs. Making it part of
+    # the type definition allows adding new arguments later.
     def fetchToFile(self, path: str, **kwargs) -> None:
         pass
 
+    # onOpen: receive the StateFile object for last-minute, backend
+    # specific changes to the state file.
+    # Note: no arguments will be passed over kwargs. Making it part of
+    # the type definition allows adding new arguments later.
     def onOpen(self, sf: nixops.statefile.StateFile, **kwargs) -> None:
         sf.create_deployment()
 
-    # uploadFromFile: upload the new state file and release any locks
+    # uploadFromFile: upload the new version of the state file
     # Note: no arguments will be passed over kwargs. Making it part of
     # the type definition allows adding new arguments later.
     def uploadFromFile(self, path: str, **kwargs) -> None:
