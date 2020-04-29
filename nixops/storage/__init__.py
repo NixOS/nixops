0f1d68726fa1a0241401b46c72bdb21bb4f6cff5
Graham Christensen <graham@grahamc.com>
2020-04-29 15:14:23 -0400

    fixup d2f845a9f5c47d2e1f5bcbdf30583c4f5d655bb5 with no locking indications

--- nixops/storage/__init__.py
+++ nixops/storage/__init__.py
@@ -33,21 +33,20 @@ class StorageBackend(Protocol[T]):
     def __init__(self, args: T) -> None:
         raise NotImplementedError
 
-    # fetchToFile: acquire a lock and download the state file to
-    # the local disk. Note: no arguments will be passed over kwargs.
-    # Making it part of the type definition allows adding new
-    # arguments later.
+    # fetchToFile: download the state file to the local disk.
+    # Note: no arguments will be passed over kwargs. Making it part of
+    # the type definition allows adding new arguments later.
     def fetchToFile(self, path: str, **kwargs) -> None:
         raise NotImplementedError
 
     # onOpen: receive the StateFile object for last-minute, backend
     # specific changes to the state file.
-    # Note: no arguments will be passed over kwargs. Making it part
-    # of the type definition allows adding new arguments later.
+    # Note: no arguments will be passed over kwargs. Making it part of
+    # the type definition allows adding new arguments later.
     def onOpen(self, sf: nixops.statefile.StateFile, **kwargs) -> None:
         pass
 
-    # uploadFromFile: upload the new state file and release any locks
+    # uploadFromFile: upload the new version of the state file
     # Note: no arguments will be passed over kwargs. Making it part of
     # the type definition allows adding new arguments later.
     def uploadFromFile(self, path: str, **kwargs) -> None:
