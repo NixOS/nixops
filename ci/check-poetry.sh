1edf569a4fd7715e6a848fe269bbcbcc31e1a352
Graham Christensen <graham@grahamc.com>
2020-05-07 09:19:16 -0400

    ci: verify poetry.lock and a docs-specific requirements.txt is consistent

---
+++ ci/check-poetry.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env nix-shell
+#!nix-shell ../shell.nix -i bash
+
+set -eu
+
+set -x
+poetry lock
+poetry export --dev -f requirements.txt > doc/requirements.txt
+git diff --exit-code poetry.lock doc/requirements.txt
