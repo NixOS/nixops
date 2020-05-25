7b36809ce556ba903a00a4b49b4e6b5cedb729ba
adisbladis <adisbladis@gmail.com>
2020-05-25 22:13:06 +0100

    Add a Local machine type based on the local transport

---
+++ nixops/backends/local.py
@@ -0,0 +1,19 @@
+from nixops.transports.local import LocalTransport
+from .none import NoneDefinition, NoneState
+from nixops.backends import Transport
+from typing import Type
+
+
+class LocalDefinition(NoneDefinition):
+    @classmethod
+    def get_type(cls):
+        return "local"
+
+
+class LocalState(NoneState):
+
+    transport_type: Type[Transport] = LocalTransport
+
+    @classmethod
+    def get_type(cls):
+        return "local"
