8a3169c2573c27fa6cea02c3f525c80608fc4f47
adisbladis <adisbladis@gmail.com>
2020-06-10 17:13:34 +0200

    flake.nix: Use projectDir

--- flake.nix
+++ flake.nix
@@ -10,14 +10,7 @@
       overrides = import ./overrides.nix { inherit pkgs; };
 
     in pkgs.poetry2nix.mkPoetryApplication {
-      # Once the latest poetry2nix release has reached 20.03 use projectDir instead of:
-      # - src
-      # - pyproject
-      # - poetrylock
-
-      src = pkgs.lib.cleanSource ./.;
-      pyproject = ./pyproject.toml;
-      poetrylock = ./poetry.lock;
+      projectDir = ./.;
 
       propagatedBuildInputs = [
         pkgs.openssh
