5a36c7b77c96ea0f843aed9a2e1e0ea451635886
Antoine Eiche <lewo@abesis.fr>
2020-06-10 18:13:57 +0200

    Pin flake-compat

--- shell.nix
+++ shell.nix
@@ -1,3 +1,6 @@
-(import (fetchTarball https://github.com/edolstra/flake-compat/archive/master.tar.gz) {
-  src = builtins.fetchGit ./.;
+(import (
+  fetchTarball {
+    url = https://github.com/edolstra/flake-compat/archive/f012cc5092f01fc67d5cd6f04999f964c3e05cbf.tar.gz;
+    sha256 = "1n8q7v7alq802kl3b6zan6v27whi2ppbnlv8df6cz64vqf658ija"; }) {
+      src = builtins.fetchGit ./.;
 }).shellNix
