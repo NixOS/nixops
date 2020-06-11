ac746bc051384254b1b472998604652db0d683a8
Antoine Eiche <lewo@abesis.fr>
2020-06-11 12:24:26 +0200

    Add a flake check running a linter on the doc
    The main objective is to be able to locally run what the CI is
    running. Now, to run the doc linter, a user just have to run `nix
    flake check`.
    
    The `lint-docs` script is now available in the shell environment.

--- flake.nix
+++ flake.nix
@@ -7,6 +7,23 @@
 
   outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system: let
     pkgs = import nixpkgs { inherit system; };
+
+    sphinx = pkgs.python3.withPackages(p: [ p.sphinx ]);
+    linters.doc = pkgs.writers.writeBashBin "lint-docs" ''
+      set -eux
+      # When running it in the Nix sandbox, there is no git repository
+      # but sources are filtered.
+      if [ -d .git ];
+      then
+          FILES=$(${pkgs.git}/bin/git ls-files)
+      else
+          FILES=$(find .)
+      fi
+      echo "$FILES" | xargs ${pkgs.codespell}/bin/codespell -L keypair,iam,hda
+      ${sphinx}/bin/sphinx-build -M clean doc/ doc/_build
+      ${sphinx}/bin/sphinx-build -n doc/ doc/_build
+      '';
+
   in {
     devShell = pkgs.mkShell {
       buildInputs = [
@@ -16,8 +33,9 @@
         pkgs.openssh
         pkgs.poetry
         pkgs.rsync  # Included by default on NixOS
+        pkgs.nixFlakes
         pkgs.codespell
-      ];
+      ] ++ (builtins.attrValues linters);
 
       shellHook = ''
         export PATH=${builtins.toString ./scripts}:$PATH
@@ -50,6 +68,19 @@
         cp ${(import ./doc/manual { revision = "1.8"; inherit nixpkgs; system = "x86_64-linux"; }).optionsDocBook} doc/manual/machine-options.xml
 
         make -C doc/manual install docdir=$out/share/doc/nixops mandir=$out/share/man
+      '';
+    };
+
+    checks.doc = pkgs.stdenv.mkDerivation {
+      name = "lint-docs";
+      # we use cleanPythonSources because the default gitignore
+      # implementation doesn't support the restricted evaluation
+      src = pkgs.poetry2nix.cleanPythonSources {
+        src = ./.;
+      };
+      dontBuild = true;
+      installPhase = ''
+        ${linters.doc}/bin/lint-docs | tee $out
       '';
     };
   });
