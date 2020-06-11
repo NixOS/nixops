3ff09af04c913670dda6f0404ba169ce0017f969
Antoine Eiche <lewo@abesis.fr>
2020-06-11 14:40:45 +0200

    ci: run the doc linter with Nix

--- ci/lint-docs.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env nix-shell
-#!nix-shell ../shell.nix -i bash
-
-set -eux
-
-git ls-files | xargs codespell -L keypair,iam,hda
-sphinx-build -M clean doc/ doc/_build
-sphinx-build -n doc/ doc/_build
