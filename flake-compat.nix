a731b0b2b16e3ef8ce400c01d553868112223815
adisbladis <adisbladis@gmail.com>
2020-06-15 10:49:51 +0200

    Use flake-compat without git
    This fixes 2 different issues:
    1. Github actions Checkout does a shallow clone by default.
       We want to use a shallow clone but force a deep clone because
       builtins.fetchGit doesn't seem to work on shallow clones.
    
    2. It doesn't work when this repo is a store path and .git is removed.

---
+++ flake-compat.nix
@@ -0,0 +1,10 @@
+{ pkgs }:
+
+(import (
+  fetchTarball {
+    url = https://github.com/edolstra/flake-compat/archive/f012cc5092f01fc67d5cd6f04999f964c3e05cbf.tar.gz;
+    sha256 = "1n8q7v7alq802kl3b6zan6v27whi2ppbnlv8df6cz64vqf658ija"; }) {
+      src = pkgs.poetry2nix.cleanPythonSources {
+        src = ./.;
+      };
+})
--- shell.nix
+++ shell.nix
@@ -1,6 +1,2 @@
-(import (
-  fetchTarball {
-    url = https://github.com/edolstra/flake-compat/archive/f012cc5092f01fc67d5cd6f04999f964c3e05cbf.tar.gz;
-    sha256 = "1n8q7v7alq802kl3b6zan6v27whi2ppbnlv8df6cz64vqf658ija"; }) {
-      src = builtins.fetchGit ./.;
-}).shellNix
+{ pkgs ? import <nixpkgs> {} }:
+(import ./flake-compat.nix { inherit pkgs; }).shellNix
