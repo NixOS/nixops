e4873c3428a88888a5c4af6100ed24e42bd9fc6e
adisbladis <adisbladis@gmail.com>
2020-07-17 19:09:08 +0200

    Remove obsolete tests

--- tests/functional/test_rollback_rollsback.py
+++ tests/functional/test_rollback_rollsback.py
@@ -5,6 +5,8 @@ from tests.functional import single_machine_test
 
 from nixops.ssh_util import SSHCommandFailed
 
+from nixops.evaluation import NetworkFile
+
 parent_dir = path.dirname(__file__)
 
 has_hello_spec = "%s/single_machine_has_hello.nix" % (parent_dir)
@@ -17,13 +19,14 @@ class TestRollbackRollsback(single_machine_test.SingleMachineTest):
 
     def setup(self):
         super(TestRollbackRollsback, self).setup()
+        self.depl.network_expr = NetworkFile(rollback_spec)
         self.depl.nix_exprs = self.depl.nix_exprs + [rollback_spec]
 
     def run_check(self):
         self.depl.deploy()
         with tools.assert_raises(SSHCommandFailed):
             self.check_command("hello")
-        self.depl.nix_exprs = self.depl.nix_exprs + [has_hello_spec]
+        self.depl.network_expr = NetworkFile(has_hello_spec)
         self.depl.deploy()
         self.check_command("hello")
         self.depl.rollback(generation=1)
