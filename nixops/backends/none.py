c3cdf78a19c0d3cff1cc0a2362e1cc304ec9253b
adisbladis <adisbladis@gmail.com>
2020-07-18 02:54:29 +0200

    nixops/backends/none.py: Add more static typing

--- nixops/backends/none.py
+++ nixops/backends/none.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
-from typing import Optional
+from typing import Optional, List
 import nixops.util
 
 from nixops.backends import MachineDefinition, MachineState, MachineOptions
 from nixops.util import attr_property, create_key_pair
+from nixops.state import RecordId
 import nixops.resources
+
+import nixops
 
 
 class NoneDefinition(MachineDefinition):
@@ -16,7 +19,7 @@ class NoneDefinition(MachineDefinition):
     config: MachineOptions
 
     @classmethod
-    def get_type(cls):
+    def get_type(cls) -> str:
         return "none"
 
     def __init__(self, name: str, config: nixops.resources.ResourceEval):
@@ -29,16 +32,18 @@ class NoneState(MachineState[NoneDefinition]):
     """State of a trivial machine."""
 
     @classmethod
-    def get_type(cls):
+    def get_type(cls) -> str:
         return "none"
 
-    target_host = nixops.util.attr_property("targetHost", None)
-    public_ipv4 = nixops.util.attr_property("publicIpv4", None)
+    target_host: str = nixops.util.attr_property("targetHost", None)
+    public_ipv4: Optional[str] = nixops.util.attr_property("publicIpv4", None)
     _ssh_private_key: Optional[str] = attr_property("none.sshPrivateKey", None)
     _ssh_public_key: Optional[str] = attr_property("none.sshPublicKey", None)
-    _ssh_public_key_deployed = attr_property("none.sshPublicKeyDeployed", False, bool)
+    _ssh_public_key_deployed: bool = attr_property(
+        "none.sshPublicKeyDeployed", False, bool
+    )
 
-    def __init__(self, depl, name, id):
+    def __init__(self, depl: "nixops.deployment.Deployment", name: str, id: RecordId):
         MachineState.__init__(self, depl, name, id)
 
     @property
@@ -68,7 +73,7 @@ class NoneState(MachineState[NoneDefinition]):
         check: bool,
         allow_reboot: bool,
         allow_recreate: bool,
-    ):
+    ) -> None:
         assert isinstance(defn, NoneDefinition)
         self.set_common_state(defn)
         self.target_host = defn._target_host
@@ -76,22 +81,24 @@ class NoneState(MachineState[NoneDefinition]):
 
         if not self.vm_id:
             if self.provision_ssh_key:
-                self.log_start("generating new SSH key pair... ")
+                self.logger.log_start("generating new SSH key pair... ")
                 key_name = "NixOps client key for {0}".format(self.name)
                 self._ssh_private_key, self._ssh_public_key = create_key_pair(
                     key_name=key_name
                 )
 
-            self.log_end("done")
+            self.logger.log_end("done")
             self.vm_id = "nixops-{0}-{1}".format(self.depl.uuid, self.name)
 
-    def switch_to_configuration(self, method, sync, command=None):
+    def switch_to_configuration(
+        self, method: str, sync: bool, command: Optional[str] = None
+    ) -> int:
         res = super(NoneState, self).switch_to_configuration(method, sync, command)
         if res == 0:
             self._ssh_public_key_deployed = True
         return res
 
-    def get_ssh_name(self):
+    def get_ssh_name(self) -> str:
         assert self.target_host
         return self.target_host
 
@@ -102,7 +109,7 @@ class NoneState(MachineState[NoneDefinition]):
             return self.write_ssh_private_key(self._ssh_private_key)
         return None
 
-    def get_ssh_flags(self, *args, **kwargs):
+    def get_ssh_flags(self, *args, **kwargs) -> List[str]:
         super_state_flags = super(NoneState, self).get_ssh_flags(*args, **kwargs)
         if self.vm_id and self.cur_toplevel and self._ssh_public_key_deployed:
             key_file = self.get_ssh_private_key_file()
@@ -125,6 +132,6 @@ class NoneState(MachineState[NoneDefinition]):
         if res.is_up:
             super()._check(res)
 
-    def destroy(self, wipe=False):
+    def destroy(self, wipe: bool = False) -> bool:
         # No-op; just forget about the machine.
         return True
