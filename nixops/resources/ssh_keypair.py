e48422a9b0d2f4db38af7db7edc7c13f799a656b
adisbladis <adisbladis@gmail.com>
2020-07-18 02:54:30 +0200

    nixops/resources/ssh_keypair.py: Add more static typing

--- nixops/resources/ssh_keypair.py
+++ nixops/resources/ssh_keypair.py
@@ -1,7 +1,9 @@
-# -*- coding: utf-8 -*-
+from __future__ import annotations
 
 # Automatic provisioning of SSH key pairs.
+from typing import Type, Dict, Optional
 
+from nixops.state import RecordId
 import nixops.util
 import nixops.resources
 
@@ -12,17 +14,17 @@ class SSHKeyPairDefinition(nixops.resources.ResourceDefinition):
     config: nixops.resources.ResourceOptions
 
     @classmethod
-    def get_type(cls):
+    def get_type(cls: Type[SSHKeyPairDefinition]) -> str:
         return "ssh-keypair"
 
     @classmethod
-    def get_resource_type(cls):
+    def get_resource_type(cls: Type[SSHKeyPairDefinition]) -> str:
         return "sshKeyPairs"
 
     def __init__(self, name: str, config: nixops.resources.ResourceEval):
         super().__init__(name, config)
 
-    def show_type(self):
+    def show_type(self) -> str:
         return "{0}".format(self.get_type())
 
 
@@ -32,14 +34,14 @@ class SSHKeyPairState(nixops.resources.ResourceState[SSHKeyPairDefinition]):
     state = nixops.util.attr_property(
         "state", nixops.resources.ResourceState.MISSING, int
     )
-    public_key = nixops.util.attr_property("publicKey", None)
-    private_key = nixops.util.attr_property("privateKey", None)
+    public_key: Optional[str] = nixops.util.attr_property("publicKey", None)
+    private_key: Optional[str] = nixops.util.attr_property("privateKey", None)
 
     @classmethod
-    def get_type(cls):
+    def get_type(cls: Type[SSHKeyPairState]) -> str:
         return "ssh-keypair"
 
-    def __init__(self, depl, name, id):
+    def __init__(self, depl: "nixops.deployment.Deployment", name: str, id: RecordId):
         nixops.resources.ResourceState.__init__(self, depl, name, id)
         self._conn = None
 
@@ -49,7 +51,7 @@ class SSHKeyPairState(nixops.resources.ResourceState[SSHKeyPairDefinition]):
         check: bool,
         allow_reboot: bool,
         allow_recreate: bool,
-    ):
+    ) -> None:
         # Generate the key pair locally.
         if not self.public_key:
             (private, public) = nixops.util.create_key_pair(type="ed25519")
@@ -58,11 +60,11 @@ class SSHKeyPairState(nixops.resources.ResourceState[SSHKeyPairDefinition]):
                 self.private_key = private
                 self.state = nixops.resources.ResourceState.UP
 
-    def prefix_definition(self, attr):
+    def prefix_definition(self, attr) -> Dict:
         return {("resources", "sshKeyPairs"): attr}
 
-    def get_physical_spec(self):
+    def get_physical_spec(self) -> Dict[str, Optional[str]]:
         return {"privateKey": self.private_key, "publicKey": self.public_key}
 
-    def destroy(self, wipe=False):
+    def destroy(self, wipe: bool = False) -> bool:
         return True
