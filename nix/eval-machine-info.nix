c9dece505c9ea97f5a10cbe162dacb928bcf336a
paki23 <pk93@posteo.net>
2022-03-13 19:13:37 +0100

    network.storage option is now defined

--- nix/eval-machine-info.nix
+++ nix/eval-machine-info.nix
@@ -15,7 +15,7 @@ let
     map (name: { inherit name; value = builtins.catAttrs name set; }) (builtins.concatMap builtins.attrNames set));
 
   flake = builtins.getFlake flakeUri;
-  flakeExpr = (builtins.tryEval flake.outputs.nixopsConfigurations.default).value or { };
+  flakeExpr = flake.outputs.nixopsConfigurations.default or { };
 
   nixpkgsBoot = toString <nixpkgs> ; # this will be replaced on install by nixops' nixpkgs input
   libBoot = import "${nixpkgsBoot}/lib";
@@ -26,24 +26,31 @@ let
         type = lib.types.path;
         description = "Path to the nixpkgs instance used to buld the machines.";
         defaultText = lib.literalDocBook "The 'nixpkgs' input to either the provided flake or nixops' own.";
-        default = (builtins.tryEval flake.inputs.nixpkgs).value or nixpkgsBoot;
+        default = flake.inputs.nixpkgs or nixpkgsBoot;
       };
-      config._module.freeformType = with lib.types;attrsOf anything;
     }
     flakeExpr
   ] ++ networkExprs;
 
-  evalBoot = libBoot.evalModules {
+  inherit ((libBoot.evalModules {
     specialArgs = args;
-    modules = baseMods libBoot;
-  };
+    modules = baseMods libBoot ++ [{
+      _module.freeformType = with libBoot.types;attrsOf anything;
+    }];
+  }).config) nixpkgs;
 
-  inherit (evalBoot.config) nixpkgs;
-
-  pkgs = (builtins.tryEval nixpkgs.legacyPackages.${system}).value or (import nixpkgs { inherit system; });
+  pkgs = nixpkgs.legacyPackages.${system} or (import nixpkgs { inherit system; });
   lib = nixpkgs.lib or pkgs.lib or (builtins.tryEval (import "${nixpkgs}/lib")).value or libBoot;
 
   inherit (lib) mkOption types;
+
+  deferredModule = with lib; mkOptionType {#TODO: remove after merging https://github.com/NixOS/nixpkgs/pull/163617
+    name = "deferredModule";
+    description = "module";
+    check = t: isAttrs t || isFunction t || builtins.isPath t;
+    merge = loc: defs: map (def: lib.setDefaultModuleLocation "${showOption loc} from ${def.file}" def.value) defs;
+  };
+  # inherit (types) deferredModule;
 in rec {
   inherit nixpkgs;
 
@@ -63,6 +70,13 @@ in rec {
               description = "Extra inputs to be passed to every node.";
               type = with types;attrsOf anything;
               default = {};
+            };
+            storage = mkOption {
+              description = "Configuring how to store the network state.";
+              default = {};
+              type = with types; submodule {
+                _module.freeformType = attrsOf anything;
+              };
             };
           };
           resources = mkOption {
@@ -111,6 +125,7 @@ in rec {
           nodes = mkOption {
             description = "The NixOS configurations for the nodes in the network.";
             type = types.attrsOf (import "${nixpkgs}/nixos/lib/eval-config.nix" {
+              inherit system lib;
               specialArgs = {
                 inherit uuid deploymentName;
                 inherit (config) nodes resources;
@@ -131,47 +146,40 @@ in rec {
                     networking.hostName = lib.mkOverride 900 name;
                     deployment.targetHost = lib.mkOverride 900 name;
                     environment.checkConfigurationOptions = lib.mkOverride 900 checkConfigurationOptions;
-                    nixpkgs.system = lib.mkDefault system;
                   })
                 ];
             }).type;
           };
           defaults = mkOption {
-            type = with lib; mkOptionType {#TODO: remove after merging https://github.com/NixOS/nixpkgs/pull/163617
-              name = "deferredModule";
-              description = "module";
-              check = t: isAttrs t || isFunction t || builtins.isPath t;
-              merge = loc: defs: map (def: lib.setDefaultModuleLocation "${showOption loc} from ${def.file}" def.value) defs;
-            };
-            # type = types.deferredModule;
+            type = deferredModule;
             default = { };
             description = ''
               Extra NixOS options to add to all nodes.
             '';
           };
         };
-        config = let
+        config.nodes = let
           nodes = removeAttrs config (builtins.attrNames options);
-        in lib.mkIf ({} != nodes) { #TODO: actual warning/assert module impl.
-          nodes = lib.warn "Please use the actual nodes.* option instead of assigning machines to the config's top level" nodes;
-        };
+        in lib.mkIf ({} != nodes) (lib.mapAttrs (n: imports:#TODO: actual warning/assert module impl.
+          lib.warn "Please use nodes.${n} option instead of assigning machines to the config's top level"
+          { inherit imports; }) nodes);
       })
+      { _module.freeformType = types.attrsOf deferredModule; }
     ];
   };
 
+  # for backward compatibility
+  network = lib.mapAttrs (n: v: [v]) net.config;
+  networks = [ net.config ];
+
   inherit (net.config) resources;
   defaults = [ net.config.defaults ];
-  #TODO: take options and other modules outputs for each node
-  nodes =
+  nodes = #TODO: take options and other modules outputs for each node
     lib.mapAttrs (n: v: {
       config = v;
       options = net.options.nodes.${n};
       inherit (v.nixpkgs) pkgs;
     }) net.config.nodes;
-
-  # for backward compatibility
-  network = lib.mapAttrs (n: v: [v]) net.config;
-  networks = [ net.config ];
 
   importedPluginNixExprs = map
     (expr: import expr)
